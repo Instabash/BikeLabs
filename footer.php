83dc66cb704597a38285a49ca5386f62e0bb5c56
Instabash <johnathan.lobo@gmail.com>
2018-11-17 18:12:18 +0500

    new files added

---
+++ footer.php
@@ -0,0 +1,14 @@
+	<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <a href="index.php"> BikeLabs.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+<script src="script/main.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+include_once 'header.php'
+?>
+<!-- Index section -->
+<div id="showcase">
+	<div class="section-main container">
+		<h1>Bikelabs</h1>
+		<h2>All of your biking needs in one place.</h2>
+		<p class="lead hide-on-small">
+			Bikelabs is the best way to experience all of your 
+			motorbike related needs in one centralized hub. 
+		</p>
+		<p class="lead hide-on-small">
+			Why waste time searching for mechanics, when the 
+			mechanic can come to you.
+		</p>
+	</div>
+</div>
+<!-- Modify section -->
+<section id="modify" class="section">
+	<div class="container">
+		<h2 class="section-head">
+			<i class="fas fa-cogs"></i> Modification
+		</h2>
+		<p class="lead">Modify your motorbike according to your specific preferences.</br> Add or remove parts from and to your motorbike, change the chassis, or alter the engine.</p></br>
+		<a href="#" class="btn btn-primary">Start Modifying Now</a>
+		<img class="border-new border border-dark rounded" src="images/modify.jpg">
+	</div>
+</section>
+<!-- Index section -->
+
+<!-- Index section -->
+<?php
+include_once 'footer.php'
+?>
