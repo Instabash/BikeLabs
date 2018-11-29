b18d76d3c95d59232c6945f8b0adebfa0dc9d966
Instabash <johnathan.lobo@gmail.com>
2018-11-29 20:00:35 +0500

    changed the h1's of alteration and modification sections

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include_once 'header.php'
 <section id="modify" class="section">
 	<div class="container">
 		<h2 class="section-head">
-			<i class="fas fa-cogs"></i> <h1 class="unselectable">Modification</h1>
+			<i class="fas fa-cogs"></i> <h1 class="unselectable">Body Modification</h1>
 		</h2>
 		<p class="lead">Modify your motorbike according to your specific preferences.</br> Add or remove parts from and to your motorbike, change the chassis, or alter the engine.</p>
 		<a href="#purchase" class="btn btn-outline-danger mb mt">Start Modifying Now</a>
@@ -32,6 +32,25 @@ include_once 'header.php'
 	</div>
 </section>
 <hr>
+
+<!-- Alteration section -->
+
+<section id="modify" class="section">
+	<div class="container">
+		<h2 class="section-head">
+			<i class="fas fa-cogs"></i> <h1 class="unselectable">Engine Alteration</h1>
+		</h2>
+		<p class="lead">Modify your motorbike according to your specific preferences.</br> Add or remove parts from and to your motorbike, change the chassis, or alter the engine.</p>
+		<a href="#purchase" class="btn btn-outline-danger mb mt">Start Modifying Now</a>
+		<div class='slider border-new border border-dark rounded'>
+		  <div class='slide1'></div>
+		  <div class='slide2'></div>
+		  <div class='slide3'></div>
+		</div>
+	</div>
+</section>
+<hr>
+
 <!-- Purchasing section -->
 
 <section id="purchase" class="section">
