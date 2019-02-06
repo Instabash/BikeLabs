4f2be4fc09adfd684e5161a06e9c7f3546566fb3
Instabash <johnathan.lobo@gmail.com>
2019-02-06 16:34:09 +0500

    Added ajax for modification page

--- header.php
+++ header.php
@@ -11,9 +11,40 @@
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">	
 	<title>BikeLabs</title>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    <script>
+      $(document).ready(function(){
+        // Set form variable
+        var form = $('#ajaxform');
+        
+        // Hijack form submit
+        form.submit(function(event){
+          // Set username variable
+          var model = $('#modelselect').val(); 
+    	  var year = $('#yearselect').val(); 
+
+          // Check if username value set
+          if(model == "Choose..." || year == "Choose..."){
+          	$("#modaftersubmission").addClass("moderror");
+          	$("#modaftersubmission").html("Please choose a model or year");
+          }
+          else{
+            // Process AJAX request
+            $.post('includes/Modification.inc.php', {model: model, year: year}, function(data){
+              // Append data into #results div
+              $("#modaftersubmission").removeClass("moderror");
+              $('#modaftersubmission').html(data);
+            });
+          }
+          
+          // Prevent default form action
+          event.preventDefault();
+        });
+      });
+    </script>
 	
 </head>
 <body>
