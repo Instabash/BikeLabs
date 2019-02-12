c4ca69105901b07c4fef2deadd754a2fae4a64d2
Instabash <johnathan.lobo@gmail.com>
2019-02-12 22:27:38 +0500

    added html for second form

--- includes/Modification.inc.php
+++ includes/Modification.inc.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-
+// session_start();
+	
 	include_once 'dbh.inc.php';
 	$model = $_POST['model'];
 	$year = $_POST['year'];
@@ -32,59 +32,99 @@ session_start();
 
 		// $_SESSION['imagename'] = $imageName;
 ?>
-		<div class='row'>
-				<div class='modleft'>
-					<div class='modslider border-new border border-dark rounded'>
-						<div class='modmarginleft'>
-							<?php
-								if(isset($spec1) && !empty($spec1))
-								{
-									$imagename1 =  "images/".$imageName.".jpg";
-									echo "<img id = 'modimage' src= $imagename1>";
-								}
-								else 
-								{
-									echo "";
-								}	
-							?>
-							
-							
-						</div>
-					</div>
-				</div>
-				<div class='modright'>
-					<div class='modmarginleft'>
-						<h4>Specifications</h4>
-					</div>
-					<div class='modmarginleft'>
-						<?php
-								if(isset($spec1) && !empty($spec1))
-									{
-										echo "</br>";
-										echo "Company : ". $spec1;
-										echo "</br>";
-										echo "Condition : " . $spec3;
-										echo "</br>";
-										echo "Price : " . $spec5;
-										echo "</br>";
-										echo "Description : " .  $spec6;
-										session_unset();
-										session_destroy();
-									}
-									else {
-										echo "";
-									}
-							?>
-				</div>
-				</div>
+
+
+
+<div class='row'>
+	<div class='modleft'>
+		<div class='modslider border-new border border-dark rounded'>
+			<div class='modmarginleft'>
+				<?php
+					if(isset($spec1) && !empty($spec1))
+					{
+						$imagename1 =  "images/".$imageName.".jpg";
+						echo "<img id = 'modimage' src= $imagename1>";
+					}
+					else 
+					{
+						echo "";
+					}	
+				?>
 			</div>
-			<div class='modmain' >
-				<div class='modmarginleft'>
-					<h4>Does this look right to you?</h4>	
-				</div>	
-				<div class='modmarginleft'>
-					<button type='submit' name='btnmod2' class='btn btn-danger' style='margin: 10px;' value=''>Yes</button> 
-					<button type='submit' name='btnmod2' class='btn btn-danger' style='margin: 10px;' value=''>No</button> 
-				</div> 
-		 </div> 
+		</div>
+	</div>
+
+	<div class='modright'>
+
+		<div class='modmarginleft'>
+			<h4>Specifications</h4>
+		</div>
+
+		<div class='modmarginleft'>
+			<?php
+				if(isset($spec1) && !empty($spec1))
+				{
+					echo "</br>";
+					echo "Company : ". $spec1;
+					echo "</br>";
+					echo "Condition : " . $spec3;
+					echo "</br>";
+					echo "Price : " . $spec5;
+					echo "</br>";
+					echo "Description : " .  $spec6;
+					
+				}
+				else {
+					echo "";
+				}
+			?>
+		</div>
+
+	</div>
+
+</div>
+
+<br><br><br><br>
+
+<div class='modmain'>
+	<div class='modmarginleft'>
+		<h3>Step 2: Choose what you want to modify...</h3>	
+	</div>	<br>
+	<div class="container">
+		<form id = "modform2" action = "" method = "POST">
+			<div class="form-row ">
+				<div class="col-3" style="padding:10px;">
+					<label class="checkbox-inline"><input id="hlightcheck" type="checkbox" value="">  Headlight</label>
+				</div>
+				<div class="col-3" style="padding:10px;">
+					<label class="checkbox-inline"><input id="jumpcheck" type="checkbox" value="">  Jumps</label>
+				</div>
+				<div class="col-3" style="padding:10px;">
+					<label class="checkbox-inline"><input id="seatcheck" type="checkbox" value="">  Seat</label>
+				</div>
+				<div class="col-3" style="padding:10px;">
+					<label class="checkbox-inline"><input id="mguardcheck" type="checkbox" value="">  Mudguard</label>
+				</div>
+				<div class="col-3" style="padding:10px;">
+					<label class="checkbox-inline"><input id="colorcheck" type="checkbox" value="">  Color</label>
+				</div>
+				<div class="col-3" style="padding:10px;">
+					<label class="checkbox-inline"><input id="cspocketcheck" type="checkbox" value="">  Chain Spocket</label>
+				</div>
+				<div class="col-3" style="padding:10px;">
+					<label class="checkbox-inline"><input id="exhaustcheck" type="checkbox" value="">  Exhaust</label>
+				</div>
+				<div class="col-3" style="padding:10px;">
+					<label class="checkbox-inline"><input id="tlightcheck" type="checkbox" value="">  Tail Light</label>
+				</div>
+			</div><br>
+			<div class="modbtn1">
+				<button type="submit" name="btnmod2" id = "modbtn13" class="btn btn-danger" style="margin: 10px;" value="">Next</button>
+			</div>
+		</form>
+	</div>
+</div>
+<div class="modmain" id="modaftersubmission2">
+
+</div>
 		
