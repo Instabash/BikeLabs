a580ae2f18bf45845e399ae8a1f73ecd6b6534f9
Instabash <johnathan.lobo@gmail.com>
2019-02-12 22:26:51 +0500

    added js for second form

--- script/main.js
+++ script/main.js
@@ -1,3 +1,63 @@
-if(window.location.href.indexOf("form=filled") > -1){
-   document.getElementById("modaftersubmission").style.visibility = 'visible';
-}
+
+      $(document).ready(function(){
+        // Set form variable
+        var form = $('#modform1');
+        
+        // Hijack form submit
+        form.submit(function(event){
+          // Set variables
+          var model = $('#modelselect').val(); 
+    	  var year = $('#yearselect').val(); 
+
+          // Check if username value set
+          if(model == "Choose..." || year == "Choose..."){
+          	$("#modaftersubmission").addClass("moderror");
+          	$("#modaftersubmission").html("Please choose a model or year");
+          }
+          else{
+            // Process AJAX request
+            $.post('includes/Modification.inc.php', {model: model, year: year}, function(data){
+              // Append data into #results div
+              $("#modaftersubmission").removeClass("moderror");
+              $('#modaftersubmission').html(data);
+            });
+          }
+          
+          // Prevent default form action
+          event.preventDefault();
+        });
+        
+	        // Set form variable
+	        var form2 = $('#modform2');
+	        
+	        // Hijack form submit
+	        form2.submit(function(event){
+	          // Set variables
+		      var hlightcheck = $('#hlightcheck').val(); 
+			  var jumpcheck = $('#jumpcheck').val(); 
+			  var seatcheck = $('#seatcheck').val(); 
+			  var mguardcheck = $('#mguardcheck').val(); 
+			  var colorcheck = $('#colorcheck').val(); 
+			  var cspocketcheck = $('#cspocketcheck').val(); 
+		      var exhaustcheck = $('#exhaustcheck').val(); 
+	    	  var tlightcheck = $('#tlightcheck').val(); 
+
+
+	          // Check if username value set
+	          
+	            // Process AJAX request
+	            $.post('includes/Modification.inc.php', {hlightcheck: hlightcheck, jumpcheck: jumpcheck, seatcheck:seatcheck, mguardcheck:mguardcheck,
+	            colorcheck:colorcheck, cspocketcheck:cspocketcheck, exhaustcheck:exhaustcheck, tlightcheck:tlightcheck }, function(data){
+	              // Append data into #results div
+	              $("#modaftersubmission2").removeClass("moderror");
+	              $('#modaftersubmission2').html(data);
+	            });
+	          
+	          
+	          // Prevent default form action
+	          event.preventDefault();
+	        });
+      });
+
+
+
