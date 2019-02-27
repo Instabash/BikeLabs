5ab4e85c31a847622790b54bc5963301d8a13361
Instabash <johnathan.lobo@gmail.com>
2019-02-27 15:49:39 +0500

    Created payment page

--- Payment.php
+++ Payment.php
@@ -7,7 +7,7 @@
 <section id="payment" class="section paymentsec">
 	<div class="container">
 		<h1>Your order</h1><br>
-		<div class="order-method-div-top p-3 border-new border border-dark rounded">
+		<div class="order-method-div-top p-3 border-new border border-dark rounded" style="border-bottom: 0px !important;">
 			<label>Order method:</label>
 			<!-- <?php
 				//echo POST['ORDER-METHOD'];
@@ -15,7 +15,7 @@
 			<img src="">
 			<a href="">Change order method</a>
 		</div>
-		<div class="cart-div-second p-3 border-new border border-dark rounded">
+		<div class="cart-div-second p-3 border-new border border-dark border-top-0 rounded">
 			<!-- <?php
 				//echo POST['order-item(n)'];
 			?> -->
@@ -23,7 +23,7 @@
 		</div>
 		<div class="paymentmain" style="margin-left: 0px;margin-right: 0px">
 			<div class="p-3 mt-3 border-new border border-dark rounded paymentleft">
-				<div class="payment-method-div-third p-3 mt-3 border-new border border-dark rounded">
+				<div class="payment-method-div-third">
 					<div style="width: 98%;">
 		 				<h5>Pay using Cash on delivery</h5><br>
 		 				<p>Depending on your chosen order method above, your payment will be made using cash on delivery</p>
@@ -41,34 +41,34 @@
 				</div>
 			</div>
 
-			<div class="p-3 mt-3 border-new border border-dark rounded paymentright ">
+			<div class="p-3 mt-3 border-new border-0 border-dark rounded paymentright">
 				<h4 class="pb-3">Your cart:</h4>
 				<div class="row">
 					<div class="payment-total-left">
-						<p class="border border-dark ">Item #1:</p>
-						<p class="border border-dark border-top-0">Item #2:</p>
-						<p class="border border-dark border-top-0">Item #3:</p>
-						<p class="border border-dark border-top-0">Item #4:</p>
-						<p class="border border-dark border-top-0">Item #5:</p>
+						<p class="border border-dark border-left-0">Item #1:</p>
+						<p class="border-bottom border-right border-dark">Item #2:</p>
+						<p class="border-bottom border-right border-dark">Item #3:</p>
+						<p class="border-bottom border-right border-dark">Item #4:</p>
+						<p class="border-bottom border-right border-dark">Item #5:</p>
 					</div>
 					<div class="payment-total-right">
-						<p class="border border-dark ">4231rs</p>
-						<p class="border border-dark border-top-0">141rs</p>
-						<p class="border border-dark border-top-0">14141414rs</p>
-						<p class="border border-dark border-top-0">1414rs</p>
-						<p class="border border-dark border-top-0">111rs</p>
+						<p class="border-top border-bottom border-dark">4231rs</p>
+						<p class="border-bottom border-dark">141rs</p>
+						<p class="border-bottom border-dark">14141414rs</p>
+						<p class="border-bottom border-dark">1414rs</p>
+						<p class="border-bottom border-dark">111rs</p>
 					</div>
 				</div>
 				<div class="row mt-5">
 					<div class="payment-total-left">
 						<p class="border border-dark border-left-0">Tax:</p>
-						<p class="border border-dark border-left-0 border-top-0">Subtotal:</p>
-						<p class="border border-dark border-left-0 border-top-0">Total:</p>
+						<p class="border-right border-bottom border-dark">Subtotal:</p>
+						<p class="border-right border-bottom border-dark">Total:</p>
 					</div>
 					<div class="payment-total-right">
-						<p class="border border-dark border-right-0">41414rs</p>
-						<p class="border border-dark border-right-0 border-top-0">1414141rs</p>
-						<p class="border border-dark border-right-0 border-top-0">14141414rs</p>
+						<p class="border-top border-bottom border-dark">41414rs</p>
+						<p class="border-bottom border-dark">1414141rs</p>
+						<p class="border-bottom border-dark">14141414rs</p>
 					</div>
 				</div>
 				
