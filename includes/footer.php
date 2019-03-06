ce1e0e5fc38250723a062d69fcbdbbcec48af694
Instabash <johnathan.lobo@gmail.com>
2019-03-06 23:49:16 +0500

    Moved header and footer.php to includes folder

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+	<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">&copy; <?php echo date("Y"); ?> Copyright.
+    <a href="index.php"> BikeLabs.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+<script src="script/main.js"></script>
+</body>
+</html>
