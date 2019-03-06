7ae3d6fdadf94fc71248213d794f10d935d322f0
Instabash <johnathan.lobo@gmail.com>
2019-03-06 23:52:43 +0500

    added gallery for individual spareparts page for later

--- script/main.js
+++ script/main.js
@@ -173,6 +173,52 @@ function switch_div(show) {
 //------------------------CHECKBOX FILTER END--------------------------------------//
 
 
+//------------------------gallery start--------------------------------------//
+
+  
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+    dots[i].className = dots[i].className.replace(" redborder", "");
+  }
+  x[slideIndex-1].style.display = "";
+  dots[slideIndex-1].className += " w3-opacity-off";
+  dots[slideIndex-1].className += " redborder";  
+  
+}
+
+//------------------------gallery end--------------------------------------//
+
 
 
 
