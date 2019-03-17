ba62cc084962eeb179dd904f44153285a5e98920
Instabash <johnathan.lobo@gmail.com>
2019-03-18 01:05:36 +0500

    added die method

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -6,3 +6,8 @@ $dbPassword = "";
 $dbName = "bikelabs1";
 
 $conn = mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName);
+
+if(!$conn)
+{
+	die("Connection failed: ".mysqli_connect_error());
+}
