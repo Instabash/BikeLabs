24960f7867aa511311e8b1d0f4bed1db0688c6a2
Instabash <johnathan.lobo@gmail.com>
2019-03-21 17:39:40 +0500

    made a dropdown for login in header to make the header less cluttered in a smaller screen

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 <?php
-	if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-    } 
+if(!isset($_SESSION)) 
+{ 
+	session_start(); 
+} 
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,7 +14,7 @@
 	<title><?php if (isset($title)) {echo $title;}else {echo "BikeLabs";} ?></title>
 
 	<!--------------------------------------------------------------------CSS-------------------------------------------------->
-<!-- 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
+	<!-- 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
 	<link rel="stylesheet" type="text/css" href="/BikeLabs/css/w3.css">
 	<!-- Icon -->
 	<link rel="shortcut icon" href="../favicon.ico"> 
@@ -23,7 +23,7 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
 	<!------------------------------------------------------------------Font css------------------------------------------->
- 	<!-- <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet"> -->
+	<!-- <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet"> -->
 	<link rel="stylesheet" type="text/css" href="/BikeLabs/css/PTSans.css">
 
 	<!--------------------------------------------------------------- custom buttons --------------------------------------------------------------->
@@ -65,6 +65,8 @@
 
 	<!-- isotope script  -->
 	<script src="/BikeLabs/script/isotope.pkgd.min.js"></script>
+	
+	<script src="/BikeLabs/script/dropzone.js"></script>
 	<!---------------------------------Scripts end---------------------------------->
 
 </head>
@@ -75,70 +77,91 @@
 		</div> -->
 		
 		<nav class=" navbar navbar-expand-lg navbar-light bg-light">
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
-		  <div class="head-container collapse navbar-collapse" id="navbarSupportedContent" >
-		    <ul class="navbar-nav mr-auto">
-		    	<h2 class="unselectable" style="color: #dc3545;padding-right: 30px;">BikeLabs</h2>
-		      	<li class="nav-item active header-padding" style="color: #dc3545;">
-		        	<a class="nav-link" href="/BikeLabs/index.php">Home <span class="sr-only">(current)</span></a>
-		      	</li>
-		      	<li class="nav-item header-padding" style="color: #dc3545;">
-			        <a class="nav-link" href="/BikeLabs/spareparts.php">Purchase spare parts</a>
-		      	</li>
-		      	<li class="nav-item dropdown header-padding" style="color: #dc3545;">
-		        	<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Modify your bike </a>
-			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-			          <a class="dropdown-item" href="/BikeLabs/modification.php">Body Modification</a>
-			          <a class="dropdown-item" href="/BikeLabs/alteration.php">Engine Alteration</a>
-			        </div>
-		      	</li>
-		      	<li class="nav-item dropdown header-padding" style="color: #dc3545;">
-		        	<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Purchase bikes </a>
-			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-			          <a class="dropdown-item" href="/BikeLabs/newbikes.php">New Bikes</a>
-			          <a class="dropdown-item" href="/BikeLabs/usedbikes.php">Used Bikes</a>
-			        </div>
-		      	</li>
-		      	<li class="nav-item header-padding" style="color: #dc3545;">
-		        	<a class="nav-link" href="/BikeLabs/postad.php">Post An Advertisement</a>
-      			</li>
-		      	
-		    </ul>
-		    <?php
-		    	if(isset($_SESSION['userId']))
-		    	{
-		    		$userId = $_SESSION["userUId"];
-	    			echo 
-	    			'
-	    			<p style="margin:5px;color:black;">Logged in as : <a href="#"> '.$userId.' </a></p>
-	    			<form class="form-inline" action="includes/logout.inc.php" style="margin-left: 10px;" method="post">
-						<button type="submit" class="btn btn-default" name="logout-submit">Logout</button>
-					</form>';
-		    	}
-		    	else
-		    	{
-		    		echo 
-		    		'<form class="form-inline" action="includes/login.inc.php" style="margin-right: 10px;" method="post">
-					  	<div class="form-group">
-		                    <input type="text" class="form-control" name="mailuid" placeholder="Username/E-mail..">
-		                </div>
-		                <div class="form-group">
-		                    <input type="password" class="form-control" name="pwd" placeholder="Password..">
-		                </div>
-		                <button type="submit" class="btn btn-default" name="login-submit">Login</button>
-					</form>
-					<a href="signup.php">Signup</a>';
-		    	}
-		    ?>
-		    
-			
+			<div class="head-container collapse navbar-collapse" id="navbarSupportedContent" >
+				<ul class="navbar-nav mr-auto">
+					<h2 class="unselectable" style="color: #dc3545;padding-right: 30px;">BikeLabs</h2>
+					<li class="nav-item active header-padding" style="color: #dc3545;">
+						<a class="nav-link" href="/BikeLabs/index.php">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item header-padding" style="color: #dc3545;">
+						<a class="nav-link" href="/BikeLabs/spareparts.php">Purchase spare parts</a>
+					</li>
+					<li class="nav-item dropdown header-padding" style="color: #dc3545;">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Modify your bike </a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="/BikeLabs/modification.php">Body Modification</a>
+							<a class="dropdown-item" href="/BikeLabs/alteration.php">Engine Alteration</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown header-padding" style="color: #dc3545;">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Purchase bikes </a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="/BikeLabs/newbikes.php">New Bikes</a>
+							<a class="dropdown-item" href="/BikeLabs/usedbikes.php">Used Bikes</a>
+						</div>
+					</li>
+					<li class="nav-item header-padding" style="color: #dc3545;">
+						<a class="nav-link" href="/BikeLabs/postad.php">Post An Advertisement</a>
+					</li>
+				</ul>
+				<ul class="navbar-nav">
+					<?php
+					if(!isset($_SESSION['userId']))
+					{?>
+						<li class="nav-item header-padding" style="color: #dc3545;">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Login </a>
+							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+								<form class="px-4 py-3" action="includes/login.inc.php" method="post">
+									<div class="form-group">
+										<label for="exampleDropdownFormEmail1">Email address</label>
+										<input type="text" class="form-control" name="mailuid" placeholder="Username/E-mail..">
+									</div>
+									<div class="form-group">
+										<label for="exampleDropdownFormPassword1">Password</label>
+										<input type="password" class="form-control" name="pwd" placeholder="Password..">
+									</div>
+									<div class="form-group">
+										<div class="form-check">
+											<input type="checkbox" class="form-check-input" id="dropdownCheck">
+											<label class="form-check-label" for="dropdownCheck">
+												Remember me
+											</label>
+										</div>
+									</div>
+									<button type="submit" class="btn btn-primary" name="login-submit">Sign in</button>
+								</form>
+							<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">New around here? Sign up</a>
+								<a class="dropdown-item" href="#">Forgot password?</a>
+							</div>
+						</li>
+					<?php 
+					}
+					?>
+				</ul>
+				
+				<?php
+				if(isset($_SESSION['userId']))
+				{
+					$userId = $_SESSION["userUId"];
+					?>
+					<p style="margin:5px;color:black;">Logged in as : <a href="#"> <?php echo $userId ?> </a></p>
+						<form class="form-inline" action="includes/logout.inc.php" style="margin-left: 10px;" method="post">
+							<button type="submit" class="btn btn-default" name="logout-submit">Logout</button>
+						</form>
+				<?php
+				}
+				?>
 
-		    <form id="demo-2" style="margin-left: 10px;" >
-				<input type="search" placeholder="Search">
-			</form>
-		  </div>	
+
+
+				<form id="demo-2" style="margin-left: 10px;" >
+					<input type="search" placeholder="Search">
+				</form>
+			</div>	
 		</nav>
 	</header>
