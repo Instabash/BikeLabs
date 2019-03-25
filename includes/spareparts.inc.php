f54645d417ce377147dc527b8251321cc4cbb3ca
Instabash <johnathan.lobo@gmail.com>
2019-03-25 11:01:16 +0500

    commented code and transfered it to postad.inc.php

--- includes/spareparts.inc.php
+++ includes/spareparts.inc.php
@@ -1,28 +1,110 @@
+<?php
+	// if (isset($_POST['spsubmit'])) {
+
+	// 	$Title = $_POST['sptitle'];
+	// 	$Condition = $_POST['spcondition'];
+	// 	$Description = $_POST['spdescription'];
+	// 	$Price = $_POST['spprice'];
+	// 	$HomeName = $_POST['sphomename'];
+	// 	$PostCode = $_POST['sppcode'];
+	// 	$CountryReg = $_POST['spcountryregion'];
+	// 	$Phone = $_POST['spphone'];
+
+	// 	$file = $_FILES['files[]'];
+
+	// 	$fileName = $file['name'];
+	// 	$fileTmpName = $file['tmp_name'];
+	// 	$fileSize = $file['size'];
+	// 	$fileError = $file['error'];
+	// 	$fileType = $file['type'];
+
+	// 	$fileExt = explode('.', $fileName);
+	// 	$fileActualExt = strtolower(end($fileExt));
+
+	// 	$allowed = array('jpg', 'jpeg', 'png');
+
+	// 	if (in_array($fileActualExt, $allowed)) {
+	// 		if ($fileError === 0) {
+	// 			if ($fileSize < 10000) {
+	// 				$fileNameNew = uniqid('', true).'.'.$fileActualExt;
+	// 				$fileDestination = 'images/sparepartimg' . $fileNameNew;
+	// 				if (empty($Title) || empty($Condition) || empty($Description) || empty($Price) || empty($HomeName) || empty($PostCode) || empty($CountryReg) || empty($Phone))
+	// 				{
+	// 					header("Location: ../postad.php?error=empty");
+	// 					exit();
+	// 				}
+	// 				else
+	// 				{
+	// 					$sql = "INSERT INTO ";
+	// 				}
+	// 				// move_uploaded_file($fileTmpName, $fileDestination);
+	// 				// header("Location: ../index.php?uploadsuccess");
+	// 			}
+	// 			else
+	// 			{
+	// 				echo "Your file is too big";
+	// 			}
+	// 		}
+	// 		else {
+	// 			echo "There was an error uploading your file";
+	// 		}
+	// 	}
+	// 	else
+	// 	{
+	// 		echo "You cannot upload files of this type";
+
+	// 	}
+	// }
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!-- <?php
 
-include_once 'dbh.inc.php';
+//include_once 'dbh.inc.php';
 
-$spaartsql = "SELECT * FROM spare_parts";
-$spartresult = mysqli_query($conn, $spaartsql);
-$resultcheck = mysqli_num_rows($spartresult);
-$spartIdarray = array();
-$spartNamearray = array();
-$spartDescarray = array();
-$spartPricearray = array();
-$spartCatarray = array();
-$spartTypearray = array();
+$spaartsql //= "SELECT * FROM spare_parts";
+$spartresult// = mysqli_query($conn, $spaartsql);
+$resultcheck //= mysqli_num_rows($spartresult);
+$spartIdarray //= array();
+$spartNamearray //= array();
+$spartDescarray //= array();
+$spartPricearray //= array();
+$spartCatarray// = array();
+$spartTypearray //= array();
 
-if($resultcheck > 0)
+//if($resultcheck > 0)
 {
-	while ($row = mysqli_fetch_assoc($spartresult))
+	//while ($row = mysqli_fetch_assoc($spartresult))
 	{
-		$spartIdarray[] = $row['part_id'];
-		$spartNamearray[] = $row['part_name'];
-		$spartDescarray[] = $row['part_desc'];
-		$spartPricearray[] = $row['part_price'];
-		$spartCatarray[] = $row['part_category'];
-		$spartTypearray[] = $row['part_type'];
-
+	//	$spartIdarray[] = $row['part_id'];
+	//	$spartNamearray[] = $row['part_name'];
+	//	$spartDescarray[] = $row['part_desc'];
+	//	$spartPricearray[] = $row['part_price'];
+	//	$spartCatarray[] = $row['part_category'];
+	//	$spartTypearray[] = $row['part_type'];
+//
 		$postid = $spartIdarray;
 		$postname = $spartNamearray;
 		$postdesc = $spartDescarray;
