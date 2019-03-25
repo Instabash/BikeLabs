35ee100841c8bbaa6ea6c959408e42c1742cc5fb
Instabash <johnathan.lobo@gmail.com>
2019-03-25 10:59:13 +0500

    added javascript file to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
 	<!-- Footer -->
 <footer class="page-footer font-small blue">
+	<script src="/BikeLabs/script/main.js"></script>
 
   <!-- Copyright -->
   <div class="footer-copyright text-center py-3">&copy; <?php echo date("Y"); ?> Copyright.
