033daeab3ce72f50aab2c23cc2397897665014bc
Instabash <johnathan.lobo@gmail.com>
2019-03-26 09:37:39 +0500

    no changes made..

--- script/imageupload.js
+++ script/imageupload.js
@@ -1,47 +1,47 @@
-// $(document).ready(function() {
-//     if (window.File && window.FileList && window.FileReader) {
-//       $("#files").on("change", function(e) {
-//         var files = e.target.files,
-//         filesLength = files.length;
+$(document).ready(function() {
+    if (window.File && window.FileList && window.FileReader) {
+      $("#files").on("change", function(e) {
+        var files = e.target.files,
+        filesLength = files.length;
 
-//         var count = $("[id^=pip]").length+1;
-//         for (var i = 0; i < filesLength; i++) {
-//           if(count > 3)
-//           {
-//             document.getElementById("files").disabled = true;
-//           }
+        var count = $("[id^=pip]").length+1;
+        for (var i = 0; i < filesLength; i++) {
+          if(count > 3)
+          {
+            document.getElementById("files").disabled = true;
+          }
           
-//           var f = files[i]
-//           var fileReader = new FileReader();
-//           fileReader.onload = (function(e) {
-//             var file = e.target;
+          var f = files[i]
+          var fileReader = new FileReader();
+          fileReader.onload = (function(e) {
+            var file = e.target;
 
-//             $("<span class=\"pip\" id=\"pip\">" +
-//               "<img class=\"imageThumb\"  src=\"" + e.target.result + "\" title=\"" + f.name + "\"/>" +
-//               "<br/><span>" + f.name + "</span>" +
-//               "<br/><span class=\"remove\">Remove image</span>" +
-//               "</span>").insertAfter("#files");
-//             $(".remove").click(function(){
-//               $(this).parent(".pip").remove();
-//             });
+            $("<span class=\"pip\" id=\"pip\">" +
+              "<img class=\"imageThumb\"  src=\"" + e.target.result + "\" title=\"" + f.name + "\"/>" +
+              "<br/><span>" + f.name + "</span>" +
+              "<br/><span class=\"remove\">Remove image</span>" +
+              "</span>").insertAfter("#files");
+            $(".remove").click(function(){
+              $(this).parent(".pip").remove();
+            });
             
-//           // Old code here
-//           /*$("<img></img>", {
-//             class: "imageThumb",
-//             src: e.target.result,
-//             title: file.name + " | Click to remove"
-//           }).insertAfter("#files").click(function(){$(this).remove();});*/
+          // Old code here
+          /*$("<img></img>", {
+            class: "imageThumb",
+            src: e.target.result,
+            title: file.name + " | Click to remove"
+          }).insertAfter("#files").click(function(){$(this).remove();});*/
           
-//         });
-//       }
-//     });
-//   } else {
-//     alert("Your browser doesn't support to File API")
-//   }
-// });
+        });
+      }
+    });
+  } else {
+    alert("Your browser doesn't support to File API")
+  }
+});
 
-// function count1()
-// {
-//    var count = $("[id^=pip]").length+1;
-//    return count
-// }
+function count1()
+{
+   var count = $("[id^=pip]").length+1;
+   return count
+}
