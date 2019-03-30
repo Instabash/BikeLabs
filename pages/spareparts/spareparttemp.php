295490d05d0ddec0ea53d4561beecde366fee0ce
Instabash <johnathan.lobo@gmail.com>
2019-03-30 14:22:48 +0500

    changed form action from get to post and sent form to cartprocess.inc.php

--- pages/spareparts/spareparttemp.php
+++ pages/spareparts/spareparttemp.php
@@ -14,6 +14,7 @@ $stmt = mysqli_stmt_init($conn);
 		<div class="paymentmain" style="margin-left: 0px;margin-right: 0px">
 			<div class="paymentleft border-new border border-dark rounded mt-5 " style="">
 				<div class="w3-content w3-display-container">
+
 					<?php
 					$imgnamesql = "SELECT ad_image_name FROM post_ad_images WHERE ad_id = {$_GET["partid"]};";
 					if(!mysqli_stmt_prepare($stmt, $imgnamesql))
@@ -27,27 +28,12 @@ $stmt = mysqli_stmt_init($conn);
 
 						while ($row1 = mysqli_fetch_assoc($result1)) 
 						{
-									//echo $row['ad_image_name'];
-
 							?>
-
 							<img class="mySlides" src="../../images/sparepartimg/<?php echo $row1['ad_image_name'] ?>" style="width:100%">
-											<!-- <img class="mySlides" src="../../images/sparepartimg/<?php //echo $row1['ad_image_name'] ?>" style="width:100%;display:none">
-											<img class="mySlides" src="../../images/sparepartimg/<?php// echo $row1['ad_image_name'] ?>" style="width:100%;display:none">
-											<img class="mySlides" src="../../images/sparepartimg/<?php //echo $row1['ad_image_name'] ?>" style="width:100%;display:none">
-											<img class="mySlides" src="../../images/sparepartimg/<?php// echo $row1['ad_image_name'] ?>" style="width:100%;display:none">
-											<img class="mySlides" src="../../images/sparepartimg/<?php// echo $row1['ad_image_name'] ?>" style="width:100%;display:none"> -->
-
-											<?php 
-										}			
-									}
-					?><!-- 
-					<img class="mySlides" src="../../images/a_dvert1.jpg" style="width:100%">
-					<img class="mySlides" src="../../images/a_dvert2.jpg" style="width:100%;display:none">
-					<img class="mySlides" src="../../images/modify.jpg" style="width:100%;display:none">
-					<img class="mySlides" src="../../images/modify2.jpg" style="width:100%;display:none">
-					<img class="mySlides" src="../../images/purchase1.jpg" style="width:100%;display:none">
-					<img class="mySlides" src="../../images/purchase2.jpg" style="width:100%;display:none"> -->
+							<?php 
+						}			
+					}
+					?>
 					<button class="w3-button w3-display-left w3-black" onclick="plusDivs(-1)">&#10094;</button>
 					<button class="w3-button w3-display-right w3-black" onclick="plusDivs(1)">&#10095;</button>
 				</div>
@@ -61,169 +47,153 @@ $stmt = mysqli_stmt_init($conn);
 					}
 					else
 					{
+						$i = 0;
 						mysqli_stmt_execute($stmt);
 						$result1 = mysqli_stmt_get_result($stmt);
 						while ($row1 = mysqli_fetch_assoc($result1)) 
-							{ $i = 1;
+							{ $i++;
 								?>
 								<div class="w3-col s4">
 									<img class="demo w3-opacity w3-hover-opacity-off border-new border border-dark rounded " src="../../images/sparepartimg/<?php echo $row1['ad_image_name'] ?>" style="width:100px;cursor:pointer" onclick="currentDiv(<?php echo $i ?>)">
 								</div>
+
 								<?php 
+
 							}
 						}		
 						?>
-
-					<!-- <div class="w3-col s4">
-						<img class="demo w3-opacity w3-hover-opacity-off border-new border border-dark rounded " src="../../images/a_dvert2.jpg" style="width:100px;cursor:pointer" onclick="currentDiv(2)">
 					</div>
-					<div class="w3-col s4">
-						<img class="demo w3-opacity w3-hover-opacity-off border-new border border-dark rounded " src="../../images/modify.jpg" style="width:100px;cursor:pointer" onclick="currentDiv(3)">
-					</div>
-					<div class="w3-col s4">
-						<img class="demo w3-opacity w3-hover-opacity-off border-new border border-dark rounded " src="../../images/modify2.jpg" style="width:100px;cursor:pointer" 
-						onclick="currentDiv(4)">
-					</div>
-					<div class="w3-col s4">
-						<img class="demo w3-opacity w3-hover-opacity-off border-new border border-dark rounded " src="../../images/modify3.jpg" style="width:100px;cursor:pointer" 
-						onclick="currentDiv(5)">
-					</div>
-					<div class="w3-col s4">
-						<img class="demo w3-opacity w3-hover-opacity-off border-new border border-dark rounded " src="../../images/purchase1.jpg" style="width:100px;cursor:pointer" 
-						onclick="currentDiv(6)">
-					</div> -->
 				</div>
-			</div>
-			<div class="paymentright">
-				<div class="border-new border border-dark rounded mt-5 p-3" style="">
-					<?php
-					if($row = mysqli_fetch_assoc($result))
-					{
+				<div class="paymentright">
+					<div class="border-new border border-dark rounded mt-5 p-3" style="">
+						<?php
+						if($row = mysqli_fetch_assoc($result))
+						{
+							echo '
+							<div>
+							<h3>'.$row['ad_title'].'</h3><br>
+							<p>'.$row['ad_price'].' Rs.</p>
+							</div>
+							';
+						}
+						?>
+					</div>
+					<div class="border-new border border-dark rounded mt-5 p-3" style="">
+						<?php
+
 						echo '
 						<div>
-						<h3>'.$row['ad_title'].'</h3><br>
-						<p>'.$row['ad_price'].' Rs.</p>
+						<h3>Description</h3><br>	
+						<p>'.$row['ad_description'].'</p>
 						</div>
 						';
-					}
-					?>
-				</div>
-				<div class="border-new border border-dark rounded mt-5 p-3" style="">
-					<?php
-
-					echo '
-					<div>
-					<h3>Description</h3><br>	
-					<p>'.$row['ad_description'].'</p>
+						?>
 					</div>
-					';
-					?>
-				</div>
-				<form action="../../addresscon.php" method="get">
-					<div class="border-new border border-dark rounded mt-5 p-3">
-						<p>
-							Quantity
-						</p><br>
-						<div class="input-group" style="padding-left: 100px;padding-right: 100px;">
-							<span class="input-group-btn">
-								<button type="button" class="btn btn-default btn-number" disabled="disabled" data-type="minus" data-field="quant">
-									<span class="fas fa-minus-square"></span>
-								</button>
-							</span>
-							<input type="text" name="quant" class="form-control input-number" value="1" min="1" max="10">
-							<span class="input-group-btn">
-								<button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant">
-									<span class="fas fa-plus-square"></span>
-								</button>
-							</span>
+					<form action="../../includes/cartprocess.inc.php?partid=<?php echo $_GET["partid"] ?>" method="post">
+						<div class="border-new border border-dark rounded mt-5 p-3">
+							<p>
+								Quantity
+							</p><br>
+							<div class="input-group" style="padding-left: 100px;padding-right: 100px;">
+								<span class="input-group-btn">
+									<button type="button" class="btn btn-default btn-number" disabled="disabled" data-type="minus" data-field="quant">
+										<span class="fas fa-minus-square"></span>
+									</button>
+								</span>
+								<input type="text" name="quant" class="form-control input-number" value="1" min="1" max="10">
+								<span class="input-group-btn">
+									<button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant">
+										<span class="fas fa-plus-square"></span>
+									</button>
+								</span>
+							</div>
+							<p></p>
 						</div>
-						<p></p>
-					</div>
-					<div class="mt-3 p-3">
-						<button type="button" name="cartBtn" class="btn btn-outline-danger">Add to cart</button>
-						<button type="submit" class="btn btn-outline-danger">Buy now</button>
-					</div>
-				</form>
+						<div class="mt-3 p-3">
+							<button type="button" name="cartBtn" class="btn btn-outline-danger">Add to cart</button>
+							<button type="submit" class="btn btn-outline-danger">Buy now</button>
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
-<script type="text/javascript" src="../../script/getparameters.js"></script>
-<script type="">
-	var slideIndex = 1;
-	showDivs(slideIndex);
+	</section>
+	<script type="text/javascript" src="../../script/getparameters.js"></script>
+	<script type="">
+		var slideIndex = 1;
+		showDivs(slideIndex);
 
-	function plusDivs(n) {
-		showDivs(slideIndex += n);
-	}
+		function plusDivs(n) {
+			showDivs(slideIndex += n);
+		}
 
-	function showDivs(n) {
-		var i;
-		var x = document.getElementsByClassName("mySlides");
-		if (n > x.length) {slideIndex = 1}
-			if (n < 1) {slideIndex = x.length}
-				for (i = 0; i < x.length; i++) {
-					x[i].style.display = "none";  
-				}
-				x[slideIndex-1].style.display = "block";  
-			}
-			$('.btn-number').click(function(e){
-				e.preventDefault();
-
-				fieldName = $(this).attr('data-field');
-				type      = $(this).attr('data-type');
-				var input = $("input[name='"+fieldName+"']");
-				var currentVal = parseInt(input.val());
-				if (!isNaN(currentVal)) {
-					if(type == 'minus') {
-
-						if(currentVal > input.attr('min')) {
-							input.val(currentVal - 1).change();
-						} 
-						if(parseInt(input.val()) == input.attr('min')) {
-							$(this).attr('disabled', true);
-						}
-
-					} else if(type == 'plus') {
-
-						if(currentVal < input.attr('max')) {
-							input.val(currentVal + 1).change();
-						}
-						if(parseInt(input.val()) == input.attr('max')) {
-							$(this).attr('disabled', true);
-						}
-
+		function showDivs(n) {
+			var i;
+			var x = document.getElementsByClassName("mySlides");
+			if (n > x.length) {slideIndex = 1}
+				if (n < 1) {slideIndex = x.length}
+					for (i = 0; i < x.length; i++) {
+						x[i].style.display = "none";  
 					}
-				} else {
-					input.val(0);
+					x[slideIndex-1].style.display = "block";  
 				}
-			});
-			$('.input-number').focusin(function(){
-				$(this).data('oldValue', $(this).val());
-			});
-			$('.input-number').change(function() {
+				$('.btn-number').click(function(e){
+					e.preventDefault();
 
-				minValue =  parseInt($(this).attr('min'));
-				maxValue =  parseInt($(this).attr('max'));
-				valueCurrent = parseInt($(this).val());
+					fieldName = $(this).attr('data-field');
+					type      = $(this).attr('data-type');
+					var input = $("input[name='"+fieldName+"']");
+					var currentVal = parseInt(input.val());
+					if (!isNaN(currentVal)) {
+						if(type == 'minus') {
 
-				name = $(this).attr('name');
-				if(valueCurrent >= minValue) {
-					$(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
-				} else {
-					alert('Sorry, the minimum value was reached');
-					$(this).val($(this).data('oldValue'));
-				}
-				if(valueCurrent <= maxValue) {
-					$(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
-				} else {
-					alert('Sorry, the maximum value was reached');
-					$(this).val($(this).data('oldValue'));
-				}
+							if(currentVal > input.attr('min')) {
+								input.val(currentVal - 1).change();
+							} 
+							if(parseInt(input.val()) == input.attr('min')) {
+								$(this).attr('disabled', true);
+							}
+
+						} else if(type == 'plus') {
+
+							if(currentVal < input.attr('max')) {
+								input.val(currentVal + 1).change();
+							}
+							if(parseInt(input.val()) == input.attr('max')) {
+								$(this).attr('disabled', true);
+							}
+
+						}
+					} else {
+						input.val(0);
+					}
+				});
+				$('.input-number').focusin(function(){
+					$(this).data('oldValue', $(this).val());
+				});
+				$('.input-number').change(function() {
+
+					minValue =  parseInt($(this).attr('min'));
+					maxValue =  parseInt($(this).attr('max'));
+					valueCurrent = parseInt($(this).val());
+
+					name = $(this).attr('name');
+					if(valueCurrent >= minValue) {
+						$(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+					} else {
+						alert('Sorry, the minimum value was reached');
+						$(this).val($(this).data('oldValue'));
+					}
+					if(valueCurrent <= maxValue) {
+						$(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+					} else {
+						alert('Sorry, the maximum value was reached');
+						$(this).val($(this).data('oldValue'));
+					}
 
 
-			});
-			$(".input-number").keydown(function (e) {
+				});
+				$(".input-number").keydown(function (e) {
         // Allow: backspace, delete, tab, escape, enter and .
         if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
              // Allow: Ctrl+A
@@ -239,11 +209,11 @@ $stmt = mysqli_stmt_init($conn);
         }
     });
 
-	var query = window.location.search.substring(1);
-	var qs = parse_query_string(query);
+				var query = window.location.search.substring(1);
+				var qs = parse_query_string(query);
 	// console.log(qs.quant);	
 	var partid = qs.partid;
-	sessionStorage.setItem('partid', partid);
+	localStorage.setItem('partid', partid);
 
 </script>
 
