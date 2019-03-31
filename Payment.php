f3a01dc71c1e24a74b039ab377fae7ba4287b705
Instabash <johnathan.lobo@gmail.com>
2019-04-01 01:10:35 +0500

    added cart information from session

--- Payment.php
+++ Payment.php
@@ -1,6 +1,6 @@
 <?php
-	$title = 'Payment';
-	include_once 'includes/header.php';
+$title = 'Payment';
+include_once 'includes/header.php';
 ?>
 
 
@@ -13,7 +13,7 @@
 				//echo POST['ORDER-METHOD'];
 			?> -->
 			<img src="">
-			<a href="" id="orderredirect">Change order method</a>
+			<a href="addresscon.php" id="orderredirect">Change order method</a>
 		</div>
 		<div class="cart-div-second p-3 border-new border border-dark border-top-0 rounded">
 			<!-- <?php
@@ -25,16 +25,16 @@
 			<div class="p-3 mt-3 border-new border border-dark rounded paymentleft">
 				<div class="payment-method-div-third">
 					<div style="width: 98%;">
-		 				<h5>Pay using Cash on delivery</h5><br>
-		 				<p>Depending on your chosen order method above, your payment will be made using cash on delivery</p>
-		 				<?php
+						<h5>Pay using Cash on delivery</h5><br>
+						<p>Depending on your chosen order method above, your payment will be made using cash on delivery</p>
+						<?php
 		 					//if ($ordermethod == "pickup") {
 		 						# code...
 		 					//}
 		 					//else if($ordermethod == "dropoff"){
 		 						# code...
 		 					//}
-		 				?>
+						?>
 
 					</div>
 					
@@ -43,7 +43,17 @@
 
 			<div class="p-3 mt-3 border-new border-0 border-dark rounded paymentright">
 				<h4 class="pb-3">Your cart:</h4>
-				<div class="row">
+				<?php
+				foreach($_SESSION['cart'] as $item)
+					{
+						echo $item['product_id'];?><br><?php
+						echo $item['title'];?><br><?php
+						echo $item['price'];?><br><?php
+						echo $item['default_img'];?><br><?php
+						echo $item['quantity'];?><br><?php
+					}
+					?>
+				<!-- <div class="row">
 					<div class="payment-total-left">
 						<p class="border border-dark border-left-0">Item #1:</p>
 						<p class="border-bottom border-right border-dark">Item #2:</p>
@@ -70,7 +80,7 @@
 						<p class="border-bottom border-dark">1414141rs</p>
 						<p class="border-bottom border-dark">14141414rs</p>
 					</div>
-				</div>
+				</div> -->
 				
 				<div class="payment-btn pt-4">
 					<button type="submit" id="" name="" class="btn btn-danger" value="">Place order</button>
@@ -132,5 +142,5 @@
 
 </script>
 <?php
-	include_once 'includes/footer.php';
+include_once 'includes/footer.php';
 ?>
