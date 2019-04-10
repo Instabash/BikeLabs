b5f4ae33212376d97587599d4fef42ac63a938f4
Instabash <johnathan.lobo@gmail.com>
2019-04-10 16:51:05 +0500

    added individual prices for custom parts, and added their sum to the corresponding session variable

--- includes/moddetailprocess.inc.php
+++ includes/moddetailprocess.inc.php
@@ -15,14 +15,15 @@ if (isset($_POST['btnmod2'])) {
 			{
 				$ctmpts[] = $selectedOption;
 			}
+		}
 		$_SESSION['modcart'][] = array(
 			'selectedpkg' => $selectedpkg,
 			'paint' => $paint,
 			'description' => $specified,
-			'ctmpts' => $ctmpts,
 			'price' => $price,
 			'default_img' => $default_img);
-		}
+		
+		// print_r($_SESSION['modcart']);
 		header("Location: ../addresscon.php");
 	}
 	if ($selectedpkg == 2) {
@@ -36,19 +37,58 @@ if (isset($_POST['btnmod2'])) {
 			{
 				$ctmpts[] = $selectedOption;
 			}
+		}
 		$_SESSION['modcart'][] = array(
 			'selectedpkg' => $selectedpkg,
 			'paint' => $paint,
 			'theme' => $theme,
 			'description' => $specified,
-			'ctmpts' => $ctmpts,
 			'price' => $price,
 			'default_img' => $default_img);
-		}
+		
 		header("Location: ../addresscon.php");
 	}
-	if ($selectedpkg == 3) {
-		$price = 8000;
+	if ($selectedpkg == "custom") {
+		$removeJCPrice = 700;
+		$reflectorPrice = 1200;
+		$hidprice = 2000;
+		$removeMudgPrice = 700;
+		$shortmeterPrice = 900; 
+		$removehlightPrice = 700;
+		$bodypaintPrice = 2300;
+		$themePrice = 4000;
+
+		$priceSum = 0;
+
+		foreach($_SESSION['pkg4'] as $key=>$value)
+		{	
+			 // echo 'The value of $_SESSION['."'".$key."'".'] is '."'".$value."'".' <br />';
+			if ($value == "Remove jump cover") {
+				$priceSum+=$removeJCPrice;
+			}
+			if ($value == "Reflectors") {
+				$priceSum+=$reflectorPrice;
+			}
+			if ($value == "HID Lights") {
+				$priceSum+=$hidprice;
+			}
+			if ($value == "Remove mudguard") {
+				$priceSum+=$removeMudgPrice;
+			}
+			if ($value == "Short meter") {
+				$priceSum+=$shortmeterPrice;
+			}	
+			if ($value == "Remove headlight holders") {
+				$priceSum+=$removehlightPrice;
+			}
+			if ($value == "Body paint (User defined)") {
+				$priceSum+=$bodypaintPrice;
+			}
+			if ($value == "Add theme (User defined)") {
+				$priceSum+=$themePrice;
+			}
+		}
+		// echo $priceSum;
 		$paint = $_POST['modpaintselect'];
 		$theme = $_POST['modthemeselect'];
 		$specified = $_POST['customspecifytxtarea'];
@@ -58,7 +98,7 @@ if (isset($_POST['btnmod2'])) {
 			'paint' => $paint,
 			'theme' => $theme,
 			'description' => $specified,
-			'price' => $price,
+			'price' => $priceSum,
 			'default_img' => $default_img);
 		header("Location: ../addresscon.php");
 	}
