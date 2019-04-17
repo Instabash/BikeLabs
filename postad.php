983eca4d99ad03941c2a7cc8bd688a866621b111
Instabash <johnathan.lobo@gmail.com>
2019-04-17 22:20:29 +0500

    fixed issue regarding image previews, created two separate pages for motorbike ads and sparepart ads. link in header now gives a page with two hyperlinks to those pages

--- postad.php
+++ postad.php
@@ -13,239 +13,20 @@ include_once 'includes/header.php';
 	<div class="container">
 		<div>
 			<h4>What are you selling?</h4>
-			<div class="btncreative btn-1 btn-1a" onclick="switch_div(1);">
-				Motorbikes
-			</div>
-			<div class="btncreative btn-1 btn-1a" onclick="switch_div(2);">
-				Spare Parts
-			</div>
+			<a href="pages/postad/postadbike.php">
+				<div class="btncreative btn-1 btn-1a" >
+					Motorbikes
+				</div>
+			</a>
+			<a href="pages/postad/postadsp.php">
+				<div class="btncreative btn-1 btn-1a" >
+					Spare Parts
+				</div>
+			</a>
 		</div><br>
-		<div class="fullAlter hide" id="show_1">
-			<h4 class="p-3">Please add the following details to submit your advert</h4>
-			<div class="advertpick form-wrap clearfix border-new border border-dark rounded">
-				<form class="p-2" action="includes/postadbike.inc.php" method="post" enctype="multipart/form-data" id="bkform">
-					<div class="form-row p-2 pt-4 mb-3">
-						<label for="title">Title</label>
-						<div>
-							<div class="input-group">
-								<input type="text" class="form-control" name="bktitle" placeholder="Title" aria-label="Title" aria-describedby="basic-addon1">
-							</div>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>Condition</label>
-						<div class="select-wrap mb-2">
-							<select class="btn" name="bkcondition" id="title">
-								<option value="" disabled selected>Select</option>
-								<option value="New">New</option>
-								<option value="Used">Used</option>
-							</select>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>Make</label>
-						<div class="select-wrap mb-2">
-							<select class="btn" name="bkmake" id="title">
-								<option value="" disabled selected>Select</option>
-								<option value="Honda">Honda</option>
-								<option value="SuperPower">SuperPower</option>
-								<option value="Unique">Unique</option>
-							</select>
-						</div>
-					</div>
-					<div class="form-row p-2 pt-4 mb-3">
-						<label for="title">Year</label>
-						<div>
-							<div class="input-group">
-								<input type="text" class="form-control" name="bkyear" placeholder="Year" aria-label="Year" aria-describedby="basic-addon1">
-							</div>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>Description</label>
-						<div>
-							<div class="input-group mb-3">
-								<textarea class="form-control" rows="5" cols="50" name="bkdescription" id="description" style="resize: none;"></textarea>
-							</div>
-						</div>
-					</div>
-					<div class="form-row pl-2 pr-2 pt-2">
-						<label>Price</label>
-						<div>
-							<div class="input-group mb-3">
-								<input type="text" class="form-control" name="bkprice" placeholder="Price" aria-label="Price" aria-describedby="basic-addon1">
-							</div>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>House name/Number</label>
-						<div class="">
-							<div class="input-group mb-3">
-								<input type="text" class="form-control" name="bkhomename" placeholder="House name or number" aria-label="House name or number" aria-describedby="basic-addon1">
-							</div>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>Postcode</label>
-						<div class="">
-							<div class="input-group mb-3">
-								<input type="text" class="form-control" name="bkpcode" placeholder="Postcode" aria-label="Postcode" aria-describedby="basic-addon1">
-							</div>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>Country/Region</label>
-						<div class="select-wrap mb-3">
-							<select class="btn" name="bkcountryregion" id="title">
-								<option value="IS" selected="selected">Islamabad</option>
-								<option value="KHI">Karachi</option>
-								<option value="LH">Lahore</option>
-								<option value="RW">Rawalpindi</option>
-								<option value="PSW">Peshawar</option>
-							</select>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>Phone Number</label>
-						<div>
-							<div class="input-group mb-3">
-								<input type="text" class="form-control" name="bkphone" placeholder="Phone number" aria-label="Phone number" aria-describedby="basic-addon1">
-							</div>
-						</div>
-					</div>
-					<div class="form-row pl-2 pr-2 pb-3 field" align="left">
-						<label>Attachment Instructions</label>
-						<ul style="list-style: none;">
-							<li>
-								Allowed only files with extension (jpg, png, gif)
-							</li>
-							<li>
-								Maximum number of allowed files 10 with 300 KB for each
-							</li>
-							<li>
-								you can select files from different folders
-							</li>
-							<li>
-								<span class=" fileinput-button">
-									<br>
-									<span>Select Attachment</span>
-									<input type="file" name="files[]" id="files" multiple accept="image/jpeg, image/png, image/gif,"><br />
-								</span>
-								<output id="Filelist" style="max-width: 630px;"></output>
-							</li>
-						</ul>
-					</div>
-					<div class="addressbtn" style="float:right;padding:10px;">
-						<button type="submit" name="bksubmit" class="btn btn-outline-danger">Post the advert</button>
-					</div>
-				</form>
-			</div>
-		</div>
-		<div class="fullAlter hide" id="show_2">
-			<h4 class="p-3">Please add the following details to submit your advert</h4>
-			<div class="advertpick form-wrap clearfix border-new border border-dark rounded">
-				<form class="p-2" action="includes/postadsp.inc.php" method="post" enctype="multipart/form-data" id="spform">
-					<div class="form-row p-2 pt-4 mb-3">
-						<label for="title">Title</label>
-						<div>
-							<div class="input-group">
-								<input type="text" class="form-control" name="sptitle" placeholder="Title" aria-label="Title" aria-describedby="basic-addon1">
-							</div>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>Condition</label>
-						<div class="select-wrap mb-2">
-							<select class="btn" name="spcondition" id="title">
-								<option value="" disabled selected>Select</option>
-								<option value="New">New</option>
-								<option value="Used">Used</option>
-							</select>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>Description</label>
-						<div>
-							<div class="input-group mb-3">
-								<textarea class="form-control" rows="5" cols="50" id="description" name="spdescription" style="resize: none;"></textarea>
-							</div>
-						</div>
-					</div>
-					<div class="form-row pl-2 pr-2 pt-2">
-						<label>Price</label>
-						<div>
-							<div class="input-group mb-3">
-								<input type="text" class="form-control" name="spprice" placeholder="Price" aria-label="Price" aria-describedby="basic-addon1">
-							</div>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>House name/Number</label>
-						<div class="">
-							<div class="input-group mb-3">
-								<input type="text" class="form-control" name="sphomename" placeholder="House name or number" aria-label="House name or number" aria-describedby="basic-addon1">
-							</div>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>Postcode</label>
-						<div class="">
-							<div class="input-group mb-3">
-								<input type="text" class="form-control" name="sppcode" placeholder="Postcode" aria-label="Postcode" aria-describedby="basic-addon1">
-							</div>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>Country/Region</label>
-						<div class="select-wrap mb-3">
-							<select class="btn" name="spcountryregion" id="title">
-								<option value="IS" selected="selected">Islamabad</option>
-								<option value="KHI">Karachi</option>
-								<option value="LH">Lahore</option>
-								<option value="RW">Rawalpindi</option>
-								<option value="PSW">Peshawar</option>
-							</select>
-						</div>
-					</div>
-					<div class="form-row p-2">
-						<label>Phone Number</label>
-						<div>
-							<div class="input-group mb-3">
-								<input type="text" class="form-control" name="spphone" placeholder="Phone number" aria-label="Phone number" aria-describedby="basic-addon1">
-							</div>
-						</div>
-					</div>
-					<div class="form-row pl-2 pr-2 pb-3 field" align="left">
-						<label>Attachment Instructions</label>
-						<ul style="list-style: none;">
-							<li>
-								Allowed only files with extension (jpg, png, gif)
-							</li>
-							<li>
-								Maximum number of allowed files 10 with 300 KB for each
-							</li>
-							<li>
-								you can select files from different folders
-							</li>
-							<li>
-								<span class=" fileinput-button">
-									<br>
-									<span>Select Attachment</span>
-									<input type="file" name="files[]" id="files2" multiple accept="image/jpeg, image/png, image/gif,"><br />
-								</span>
-								<output id="Filelist2" style="max-width: 630px;"></output>
-							</li>
-						</ul>
-					</div>
-					<div class="addressbtn" style="float:right;padding:10px;">
-						<button type="submit" name="spsubmit" class="btn btn-outline-danger">Post the advert</button>
-					</div>
-				</form>
-			</div>
-		</div>
 	</div>
 </section>
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 
         //I added event handler for the file upload control to access the files properties.
         document.addEventListener("DOMContentLoaded", init, false);
@@ -533,7 +314,7 @@ include_once 'includes/header.php';
 				});
 			}));
         });
-    </script>
+</script> -->
     <?php	
     include_once 'includes/footer.php';
     ?>
