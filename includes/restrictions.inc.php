ea15bdc271f3126727e4554a16c6cea4ee9ff933
Instabash <johnathan.lobo@gmail.com>
2019-04-24 15:53:06 +0500

    added functions for restricting admin page from user

---
+++ includes/restrictions.inc.php
@@ -0,0 +1,47 @@
+<?php
+
+function logged_in(){	
+	if(!isset($_SESSION['userUId']))
+	{
+		$current_page = $_SERVER['REQUEST_URI'];
+		$_SESSION['curr_page'] = $current_page;	
+		header("Location: /BikeLabs/LoginOrRegister.php");
+		exit();
+	}
+}
+
+function admin_protect(){
+	$userId = $_SESSION['userId'];
+	if (is_admin($userId) === false) {
+		header("Location: /BikeLabs/index.php");
+		exit();
+	}
+}
+
+function is_admin($user_id)
+{
+	require 'dbh.inc.php';
+	$user_id = (int)$user_id;
+	$query = "SELECT * FROM users WHERE idUsers = ? AND User_type = 1";
+	$stmt = mysqli_stmt_init($conn);
+	// echo $user_id;
+	if (!mysqli_stmt_prepare($stmt, $query)) 
+	{
+		header("Location: /BikeLabs/index.php?error=sqlerror");
+		exit();
+	}
+	else
+	{
+		mysqli_stmt_bind_param($stmt, "s", $user_id);
+		mysqli_stmt_execute($stmt);
+		mysqli_stmt_store_result($stmt);
+		$resultCheck = mysqli_stmt_num_rows($stmt);
+		if($resultCheck > 0)
+		{
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
