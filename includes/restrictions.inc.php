39dc4d7cb1a83917abeda0f8a863947021314db0
Instabash <johnathan.lobo@gmail.com>
2019-04-26 21:16:12 +0500

    added restriction for userpanel to prevent unauthorized access

--- includes/restrictions.inc.php
+++ includes/restrictions.inc.php
@@ -45,3 +45,39 @@ function is_admin($user_id)
 		}
 	}
 }
+
+function user_protect(){
+	$userId = $_SESSION['userId'];
+	if (is_user($userId) === false) {
+		header("Location: /BikeLabs/index.php");
+		exit();
+	}
+}
+
+function is_user($user_id)
+{
+	require 'dbh.inc.php';
+	$user_id = (int)$user_id;
+	$query = "SELECT * FROM users WHERE idUsers = ? AND User_type = 0";
+	$stmt = mysqli_stmt_init($conn);
+	// echo $user_id;
+	if (!mysqli_stmt_prepare($stmt, $query)) 
+	{
+		header("Location: /BikeLabs/index.php?error=sqlerror");
+		exit();
+	}
+	else
+	{
+		mysqli_stmt_bind_param($stmt, "s", $user_id);
+		mysqli_stmt_execute($stmt);
+		mysqli_stmt_store_result($stmt);
+		$resultCheck = mysqli_stmt_num_rows($stmt);
+		if($resultCheck > 0)
+		{
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
