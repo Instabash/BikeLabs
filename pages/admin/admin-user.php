8a86feed7025d1931872ab438135cb0e67ca8762
Instabash <johnathan.lobo@gmail.com>
2019-04-27 00:58:19 +0500

    created new pages for each admin section

---
+++ pages/admin/admin-user.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+include '../../includes/restrictions.inc.php';
+admin_protect();
+include '../../includes/header.php';
+?>
+<!-- Sidebar -->
+<label href="#" class="list-group-item" style="width: auto;">Admin Panel
+	<button class="btn" id="menu-toggle"><i class="fas fa-bars"></i></button>
+</label>
+<div class="d-flex" id="wrapper">
+	<div class="bg-light border-right" id="sidebar-wrapper">
+		<div class="list-group list-group-flush">
+			<a href="/BikeLabs/pages/admin/admindash.php" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+			<a href="/BikeLabs/pages/admin/admin-vendor.php" class="list-group-item list-group-item-action bg-light">Vendor management</a>
+			<a href="/BikeLabs/pages/admin/admin-user.php" class="list-group-item list-group-item-action bg-light">User management</a>
+			<a href="/BikeLabs/pages/admin/admin-sales.php" class="list-group-item list-group-item-action bg-light">Sales</a>
+		</div>
+	</div>
+	<section id="modify" class="section modsection content">
+		<div class="container">
+			<p>sggs</p>
+		</div>
+	</section>
+</div>
+
+<script>
+	$("#menu-toggle").click(function(e) {
+		e.preventDefault();
+		$("#wrapper").toggleClass("toggled");
+	});
+</script>
+<?php
+include_once '../../includes/footer.php';
+?>
