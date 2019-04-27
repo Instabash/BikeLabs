f538fd50f000e3aa8f0ee733a07d539fd17fecf9
Instabash <johnathan.lobo@gmail.com>
2019-04-27 12:39:33 +0500

    admin can remove vendor from admin-vendor page

---
+++ includes/removevendor.inc.php
@@ -0,0 +1,23 @@
+<?php
+require 'dbh.inc.php';
+
+if (isset($_POST['submit-remove'])) {
+	if(!empty($_POST['rem-vendor-check'])) {
+		foreach($_POST['rem-vendor-check'] as $check) {
+			$sql = "DELETE FROM users WHERE idUsers=?;";
+			$stmt = mysqli_stmt_init($conn);
+
+			if (!mysqli_stmt_prepare($stmt, $sql)) 
+			{
+				header("Location: ../index.php?error=sqlerror");
+				exit();
+			}
+			else
+			{
+				mysqli_stmt_bind_param($stmt, "s", $check);
+				mysqli_stmt_execute($stmt);
+				header("Location: /BikeLabs/pages/admin/admin-vendor.php?removalsuccess");
+			}
+		}
+	}
+}
