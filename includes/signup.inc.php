53b4493e1bf191736b027c8a12d771750f0e9834
Instabash <johnathan.lobo@gmail.com>
2019-04-27 12:32:23 +0500

    added registration for vendor through admin panel

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -83,6 +83,79 @@ if(isset($_POST['signup-submit']))
 	mysqli_stmt_close($stmt);
 	mysqli_close($conn);
 } 
+elseif(isset($_POST['signup-vendor']))
+{
+	require 'dbh.inc.php';
+
+	$username = $_POST['uid'];
+	$email = $_POST['mail'];
+	$phone = $_POST['phone'];
+	$password = $_POST['pwd'];
+	$passwordRepeat = $_POST['pwd-repeat'];
+	$usertype = 2;
+	if(empty($username) || empty($email) || empty($password) || empty($passwordRepeat))
+	{
+		header("Location: /BikeLabs/pages/admin/admin-vendor.php?error=emptyfields&uid=".$username."&mail=".$email);
+		exit();
+	}
+	elseif (!filter_var($email,FILTER_VALIDATE_EMAIL)) 
+	{
+		header("Location: /BikeLabs/pages/admin/admin-vendor.php?error=invalidmail&uid=".$username);
+		exit();
+	}
+	elseif (!preg_match("/^((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$/", $phone)) 
+	{
+		header("Location: /BikeLabs/pages/admin/admin-vendor.php?error=invalidphone");
+		exit();
+	}
+	elseif ($password !== $passwordRepeat) {
+		header("Location: /BikeLabs/pages/admin/admin-vendor.php?error=passwordcheck&uid=".$username."&mail=".$email);
+		exit();
+	}
+	else
+	{
+		$sql = "SELECT uidUsers FROM users WHERE uidUsers=?";
+		$stmt = mysqli_stmt_init($conn);
+		if (!mysqli_stmt_prepare($stmt, $sql)) 
+		{
+			header("Location: /BikeLabs/pages/admin/admin-vendor.php?error=sqlerror");
+			exit();
+		}
+		else
+		{
+			mysqli_stmt_bind_param($stmt, "s", $username);
+			mysqli_stmt_execute($stmt);
+			mysqli_stmt_store_result($stmt);
+			$resultCheck = mysqli_stmt_num_rows($stmt);
+			if($resultCheck > 0)
+			{
+				header("Location: /BikeLabs/pages/admin/admin-vendor.php?error=usertaken&mail=".$email);
+				exit();
+			}
+			else
+			{
+				$sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers, User_Contact, User_type) VALUES (?, ?, ?, ?, ?)";
+				$stmt = mysqli_stmt_init($conn);
+				if (!mysqli_stmt_prepare($stmt, $sql)) 
+				{
+					header("Location: /BikeLabs/pages/admin/admin-vendor.php?error=sqlerror");
+					exit();
+				}
+				else
+				{
+					$hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+
+					mysqli_stmt_bind_param($stmt, "sssss", $username, $email, $hashedPwd, $phone, $usertype);
+					mysqli_stmt_execute($stmt);
+					header("Location: /BikeLabs/pages/admin/admin-vendor.php?signup=success");
+					exit();
+  				}
+			}
+		}
+	}
+	mysqli_stmt_close($stmt);
+	mysqli_close($conn);
+}
 else
 {
 	header("Location: ../signup.php");
