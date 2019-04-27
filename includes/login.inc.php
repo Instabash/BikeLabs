9d2cb1f4bc3111896925f3a44789a074867d3bb5
Instabash <johnathan.lobo@gmail.com>
2019-04-27 12:31:39 +0500

    allowed users to log in using their email aswell as user id

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -14,7 +14,7 @@ if (isset($_POST['login-submit']))
 	}
 	else
 	{
-		$sql = "SELECT * FROM users WHERE uidUsers=?;";
+		$sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers = ?;";
 		$stmt = mysqli_stmt_init($conn);
 		if (!mysqli_stmt_prepare($stmt, $sql)) 
 		{
@@ -23,7 +23,7 @@ if (isset($_POST['login-submit']))
 		}
 		else
 		{
-			mysqli_stmt_bind_param($stmt, "s", $mailuid);
+			mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
 			mysqli_stmt_execute($stmt);
 			$result = mysqli_stmt_get_result($stmt);
 			if($row = mysqli_fetch_assoc($result))
