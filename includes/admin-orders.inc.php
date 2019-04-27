f47a444a130b40c0204d4f77499091e5d5ce7611
Instabash <johnathan.lobo@gmail.com>
2019-04-27 12:38:00 +0500

    added page for processing admin orders, code yet to be written

---
+++ includes/admin-orders.inc.php
@@ -0,0 +1,10 @@
+<?php
+require 'dbh.inc.php';
+
+if (isset($_POST['submit-job'])) {
+	if(!empty($_POST['appr-order-check'])) {
+    foreach($_POST['appr-order-check'] as $check) {
+            
+    }
+}
+}
