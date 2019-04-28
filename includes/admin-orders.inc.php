0b47b631f8d3eedbbdd772793d59ff74b2e9ee7a
Instabash <johnathan.lobo@gmail.com>
2019-04-29 00:27:27 +0500

    admin can now assign a job to a vendor

--- includes/admin-orders.inc.php
+++ includes/admin-orders.inc.php
@@ -2,9 +2,24 @@
 require 'dbh.inc.php';
 
 if (isset($_POST['submit-job'])) {
-	if(!empty($_POST['appr-order-check'])) {
-    foreach($_POST['appr-order-check'] as $check) {
-            
-    }
-}
+	if (!empty($_POST['vendor-select'])) {
+		if(!empty($_POST['order-check'])) {
+			$selectedvendor = $_POST['vendor-select'];
+			foreach($_POST['order-check'] as $check) {
+				$sql = "UPDATE order_table SET order_status = 'Approved', assigned_vendor = '$selectedvendor' WHERE order_id = '$check'";
+				$result = mysqli_query($conn, $sql);
+				header("Location: /BikeLabs/pages/admin/admin-orders.php?successr");
+			}
+		}
+		else
+		{
+			header("Location: /BikeLabs/pages/admin/admin-orders.php?error=order");
+			exit();
+		}
+	}
+	else
+	{
+		header("Location: /BikeLabs/pages/admin/admin-orders.php?error=vendor");
+		exit();
+	}
 }
