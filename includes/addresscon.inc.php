da1c92708f3b86b3b73b6f85991edf886ec6e247
Instabash <johnathan.lobo@gmail.com>
2019-05-01 22:21:52 +0500

    added code for cart, for spare parts and bikes

--- includes/addresscon.inc.php
+++ includes/addresscon.inc.php
@@ -4,7 +4,58 @@ unset($_SESSION['modaddress']);
 unset($_SESSION['altaddress']);
 unset($_SESSION['modORalt']);
 
-if (isset($_SESSION['modcart'])) {
+if (isset($_SESSION['cart'])) {
+	if (isset($_POST['homepickbtn'])) {
+		$title = $_POST['title'];
+		$fname = $_POST['fname'];
+		$lname = $_POST['lname'];
+		$phone = $_POST['phone'];
+		$countryreg = $_POST['countryorregion'];
+		$hnameorno = $_POST['hnameorno'];
+		$pcode = $_POST['pcode'];
+		$del_method = 1;
+		if(empty($title) || empty($fname) || empty($lname) || empty($phone) || empty($countryreg) || empty($hnameorno) || empty($pcode) || empty($del_method))
+		{
+			header("Location: ../AddressCon.php?error=emptyfields&title=".$title."&fname=".$fname."&lname=".$lname."&countryreg=".$countryreg."&hnameorno=".$hnameorno."&pcode=".$pcode."&del_method=".$del_method."&phone=".$phone);
+			exit();
+		}
+		elseif (!preg_match("/^[a-zA-Z]*$/", $fname) || !preg_match("/^[a-zA-Z]*$/", $lname)) 
+		{
+			header("Location: ../AddressCon.php?error=invalidchar");
+			exit();
+		}
+		elseif (!preg_match("/^((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$/", $phone)) {
+			header("Location: ../AddressCon.php?error=invalidphone");
+			exit();
+		}
+		elseif (!preg_match("/^\\d{5}$/", $pcode)) {
+			header("Location: ../AddressCon.php?error=invalidpcode");
+			exit();
+		}
+		else{
+			if ($del_method == 1) {
+				$del_method_text = "Home Pickup";
+			}
+			if ($del_method == 2) {
+				$del_method_text = "Drop Off";
+			}
+
+			$_SESSION['del_method'] = $del_method_text;
+			$_SESSION['modaddress'][] = array(
+				'modadtitle' => $title,
+				'modadfname' => $fname,
+				'modadlname' => $lname,
+				'modadphone' => $phone,
+				'modadcountry' => $countryreg,
+				'modadhname' => $hnameorno,
+				'modadpcode' => $pcode
+			);
+			header("Location: ../payment.php");
+		}
+	}
+}
+
+elseif (isset($_SESSION['modcart'])) {
 	if (isset($_POST['homepickbtn'])) {
 		$title = $_POST['title'];
 		$fname = $_POST['fname'];
