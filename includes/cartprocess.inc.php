82e15f7c302ac48e1ca5db020900689ac7c3e9c5
Instabash <johnathan.lobo@gmail.com>
2019-05-04 11:08:03 +0500

    fixed bug where cart was increasing quantity of latest item instead of the specified item.

--- includes/cartprocess.inc.php
+++ includes/cartprocess.inc.php
@@ -2,15 +2,13 @@
 include_once 'dbh.inc.php';
 session_start();
 
-if(isset($_POST['cartBtn-bikes'])){
+if (isset($_POST['buyBtn-bikes'])) {
+
+	$quantity = 1;
 	$bike_id = $_GET["bikeid"];
-
-	// echo $bike_id;
 	$sql = "SELECT * FROM bikes WHERE bike_id='$bike_id'";
-
 	$result = mysqli_query($conn, $sql);
 	$stmt = mysqli_stmt_init($conn);
-
 	$imgnamesql = "SELECT bike_image_name FROM b_images WHERE bike_id = {$_GET["bikeid"]} AND bike_image_thumb = '0';";
 
 	if(!mysqli_stmt_prepare($stmt, $imgnamesql))
@@ -33,55 +31,17 @@ if(isset($_POST['cartBtn-bikes'])){
 		$title = $row['bike_name'];
 		$price = $row['bike_price'];
 	}
-	// echo $title;
+
 	$_SESSION['cart'][] = array(
 		'product_id' => $bike_id,
 		'title' => $title,
 		'price' => $price,
-		'default_img' => $default_img);
-	header("Location: ../cart.php?bikeid=$bike_id");
+		'default_img' => $default_img,
+		'quantity' => $quantity);
+
+	header("Location: ../addresscon.php?bikeid=$bike_id");
 }
 
-elseif (isset($_POST['buyBtn-bikes'])) {
-	if(!empty($_POST["quant"])) {
-		$quantity = $_POST["quant"];
-		$part_id = $_GET["partid"];
-		$sql = "SELECT * FROM post_ad WHERE ad_id='$part_id'";
-		$result = mysqli_query($conn, $sql);
-		$stmt = mysqli_stmt_init($conn);
-		$imgnamesql = "SELECT ad_image_name FROM post_ad_images WHERE ad_id = {$_GET["partid"]} AND ad_image_thumb = '0';";
-
-		if(!mysqli_stmt_prepare($stmt, $imgnamesql))
-		{
-			echo "SQL statement failed";
-		}
-		else
-		{
-			mysqli_stmt_execute($stmt);
-			$result1 = mysqli_stmt_get_result($stmt);
-
-			while ($row1 = mysqli_fetch_assoc($result1)) 
-			{
-				$default_img = $row1['ad_image_name'];
-			}			
-		}
-
-		if($row = mysqli_fetch_assoc($result))
-		{
-			$title = $row['ad_title'];
-			$price = $row['ad_price'];
-		}
-
-		$_SESSION['cart'][] = array(
-			'product_id' => $part_id,
-			'title' => $title,
-			'price' => $price,
-			'default_img' => $default_img,
-			'quantity' => $quantity);
-
-		header("Location: ../addresscon.php?partid=$part_id");
-	}
-}
 
 elseif(isset($_POST['cartBtn-parts'])){
 	if(!empty($_POST["quant"])) {
@@ -113,13 +73,24 @@ elseif(isset($_POST['cartBtn-parts'])){
 			$price = $row['part_price'];
 		}
 
+		$exists = false;
+		foreach ($_SESSION['cart'] as $key => $item) {
+			if ($item['product_id'] == $part_id) {
+				$exists = true;
+				break;
+			}
+		}
+		if ($exists == true) {
+			$_SESSION["cart"][$key]['quantity']+=$quantity;
+		}
+		else{
 		$_SESSION['cart'][] = array(
 			'product_id' => $part_id,
 			'title' => $title,
 			'price' => $price,
 			'default_img' => $default_img,
 			'quantity' => $quantity);
-
+		}
 		header("Location: ../cart.php?partid=$part_id");
 	}
 }
@@ -163,6 +134,10 @@ elseif (isset($_POST['buyBtn-parts'])) {
 		
 		header("Location: ../addresscon.php?partid=$part_id");
 	}
+}
+
+elseif (isset($_POST['cartBuy-parts'])) {
+	header("Location: ../addresscon.php");
 }
 
 if (isset($_POST['removeItem'])) {
