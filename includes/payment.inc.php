6d1fbfb4624f3dc0714967e0953b059d8f3e447d
Instabash <johnathan.lobo@gmail.com>
2019-05-22 00:21:40 +0500

    changed order type to parts for spare parts and bike for bikes. Also added code for bikecart session for purchasing new bikes

--- includes/payment.inc.php
+++ includes/payment.inc.php
@@ -250,8 +250,83 @@ if (isset($_POST['btnplaceorder'])) {
 		// echo "address: " .$address;
 		}
 	}
+	elseif (isset($_SESSION['bikecart'])) {
+		$ordertype = "bike";
+
+		$orderStatus = "Pending approval";
+		$orderDate = date("Y-m-d h:i:sa");
+		$userid = $_SESSION['userId'];
+
+		$total_price = 0;
+		foreach($_SESSION['bikecart'] as $item)
+		{
+			
+		}
+		
+		$del_method = $_SESSION['del_method'];
+		$pay_type = "cash on delivery";
+		foreach($_SESSION['new_b_p_address'] as $value)
+		{
+			$address = $value['new_b_p_hname'] . ", " . $value['new_b_p_pcode'] . ", " . $value['new_b_p_country'];
+			$fullname = $value['new_b_p_fname'] . " " .  $value['new_b_p_lname'];
+		}
+
+		foreach ($_SESSION['bikecart'] as $item) {
+			$price = $item['price'];
+			$orderQuantity = $item['quantity'];
+			$multipliedprice = $price*$orderQuantity;
+			$total_price+=$multipliedprice;
+		}
+		$ordertablesql = "INSERT INTO order_table (order_type, order_status, order_date, idUsers) VALUES (?, ?, ?, ?)";
+		$stmt = mysqli_stmt_init($conn);
+		if (!mysqli_stmt_prepare($stmt, $ordertablesql)) 
+		{
+			echo "SQL statement failed";
+		}
+		else
+		{
+			mysqli_stmt_bind_param($stmt, "ssss", $ordertype, $orderStatus, $orderDate, $userid);
+			mysqli_stmt_execute($stmt);
+			$orderid = $conn->insert_id;
+			$_SESSION["order_id"] = $orderid;
+
+
+			foreach ($_SESSION['bikecart'] as $item) 
+			{
+				$title = $item['title'];
+				$price = $item['price'];
+				$order_summ = $title . ", " . $price . " Rs.";
+				$part_bike_id = $item['product_id'];
+				$orderQuantity = $item['quantity'];
+
+				$orderitems = "INSERT INTO order_items (order_id, Order_price, Order_total_price, Order_quantity, Order_type_ID, Order_Address) VALUES (?, ?, ?, ?, ?, ?)";
+				if (!mysqli_stmt_prepare($stmt, $orderitems)) 
+				{
+					echo "SQL statement failed";
+				}
+				else
+				{
+					mysqli_stmt_bind_param($stmt, "ssssss", $orderid, $multipliedprice, $total_price, $orderQuantity, $part_bike_id, $address);
+					mysqli_stmt_execute($stmt);
+				}
+			}
+			$invoicesql = "INSERT INTO invoice (order_id, uidUsers, user_address, delivery_method, order_summary, payment_type, billing_address) VALUES (?, ?, ?, ?, ?, ?, ?)";
+			if (!mysqli_stmt_prepare($stmt, $invoicesql)) 
+			{
+				echo "SQL statement failed";
+			}
+			else
+			{	
+						// echo $orderid . $fullname . $address . $del_method . $order_summ . $pay_type . $address;
+				mysqli_stmt_bind_param($stmt, "sssssss", $orderid, $fullname, $address, $del_method, $order_summ, $pay_type, $address);
+				mysqli_stmt_execute($stmt);
+				unset($_SESSION['bikecart']);
+				header("Location: ../orderconfirmation.php?order=success");
+			}
+		}
+	}
 	else{
-		$ordertype = "parts or bikes";
+		$ordertype = "parts";
 
 		$orderStatus = "Pending approval";
 		$orderDate = date("Y-m-d h:i:sa");
