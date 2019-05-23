c10efd380a0fc1abfcd54a88bf5913596ee48a2b
Instabash <johnathan.lobo@gmail.com>
2019-05-23 11:10:15 +0500

    bike compare v1.1

--- includes/bikecompare.inc.php
+++ includes/bikecompare.inc.php
@@ -1,6 +1,7 @@
 <?php
 include_once 'dbh.inc.php';
 session_start();
+unset($_SESSION['bikespecs']);
 if (isset($_POST['cmpsubmit'])) {
 	
 	$Bike1Make = $_POST['category'];
@@ -28,124 +29,7 @@ if (isset($_POST['cmpsubmit'])) {
 			// array_push($arraybike, $bike_id);
 			$arraystring = "bike1=" . $results[0] . "&bike2=" . $results[1];
 			// print_r($arraybike);
-
-			$sql2 = "SELECT * FROM bikespecs WHERE bike_id = {$row['bike_id']};";
-
-			if(!mysqli_stmt_prepare($stmt, $sql2))
-			{
-				echo "SQL statement failed";
-			}
-			else
-			{
-				mysqli_stmt_execute($stmt);
-				$result1 = mysqli_stmt_get_result($stmt);
-
-				while($row1 = mysqli_fetch_assoc($result1)) 
-				{
-					$eng_type = $row1['bike_engine_type'];
-					$bore_str = $row1['bike_bore_stroke'];
-					$trans = $row1['bike_transmission'];
-					$starting = $row1['bike_starting'];
-					$frame = $row1['bike_frame'];
-					$dimensions = $row1['bike_dimensions'];
-					$petrol_cap = $row1['bike_petrol_cap'];
-					$f_tyre = $row1['bike_f_tyre'];
-					$b_tyre = $row1['bike_b_tyre'];
-					$dry_weight = $row1['bike_dry_weight'];
-				}
-				$_SESSION['bikespecs']['bike_id']= array(
-					'bike_id' => $bike_id, 
-					'eng_type' => $eng_type,
-					'bore_str' => $bore_str,
-					'trans' => $trans,
-					'starting' => $starting,
-					'frame' => $frame,
-					'dimensions' => $dimensions,
-					'petrol_cap' => $petrol_cap,
-					'f_tyre' => $f_tyre,
-					'b_tyre' => $b_tyre,
-					'dry_weight' => $dry_weight);
-				header("Location: ../pages/new-bikes/bikecompare/comparetemp.php?$arraystring");			
-			}
-		}	
-	}
+			header("Location: ../pages/new-bikes/bikecompare/comparetemp.php?$arraystring");			
+		}
+	}	
 }
-
-// $bike_id = $row['bike_id'];
-// $arraybike = array();
-// array_push($arraybike, $bike_id);
-// $arraystring = "bike1=" . $arraybike[0] . "&bike2=" . $arraybike[1];
-
-// $eng_type = $row1['bike_engine_type'];
-// $bore_str = $row1['bike_bore_stroke'];
-// $trans = $row1['bike_transmission'];
-// $starting = $row1['bike_starting'];
-// $frame = $row1['bike_frame'];
-// $dimensions = $row1['bike_dimensions'];
-// $petrol_cap = $row1['bike_petrol_cap'];
-// $f_tyre = $row1['bike_f_tyre'];
-// $b_tyre = $row1['bike_b_tyre'];
-// $dry_weight = $row1['bike_dry_weight'];
-// 					// echo $row1['bike_engine_type'];
-
-// $_SESSION['bikespecs'][$bike_id]= array(
-// 	'eng_type' => $eng_type,
-// 	'bore_str' => $bore_str,
-// 	'trans' => $trans,
-// 	'starting' => $starting,
-// 	'frame' => $frame,
-// 	'dimensions' => $dimensions,
-// 	'petrol_cap' => $petrol_cap,
-// 	'f_tyre' => $f_tyre,
-// 	'b_tyre' => $b_tyre,
-// 	'dry_weight' => $dry_weight);
-// 					// print_r($_SESSION['bikespecs']);
-// echo $arraystring;
-// header("Location: ../pages/new-bikes/bikecompare/comparetemp.php?$arraystring");			
-
-
-
-
-
-// $bike_id = $row['bike_id'];
-// 			$arraybike = array();
-// 			array_push($arraybike, $bike_id);
-// 			// $arraystring = "bike1=" . $arraybike[0] . "&bike2=" . $arraybike[1];
-// 			print_r($arraybike);
-
-// 			$sql2 = "SELECT * FROM bikespecs WHERE bike_id = {$row['bike_id']};";
-
-// 			if(!mysqli_stmt_prepare($stmt, $sql2))
-// 			{
-// 				echo "SQL statement failed";
-// 			}
-// 			else
-// 			{
-// 				mysqli_stmt_execute($stmt);
-// 				$result1 = mysqli_stmt_get_result($stmt);
-
-// 				while($row1 = mysqli_fetch_assoc($result1)) 
-// 				{
-// 					$eng_type = $row1['bike_engine_type'];
-// 					$bore_str = $row1['bike_bore_stroke'];
-// 					$trans = $row1['bike_transmission'];
-// 					$starting = $row1['bike_starting'];
-// 					$frame = $row1['bike_frame'];
-// 					$dimensions = $row1['bike_dimensions'];
-// 					$petrol_cap = $row1['bike_petrol_cap'];
-// 					$f_tyre = $row1['bike_f_tyre'];
-// 					$b_tyre = $row1['bike_b_tyre'];
-// 					$dry_weight = $row1['bike_dry_weight'];
-// 				}
-// 				$_SESSION['bikespecs'][$bike_id]= array(
-// 					'eng_type' => $eng_type,
-// 					'bore_str' => $bore_str,
-// 					'trans' => $trans,
-// 					'starting' => $starting,
-// 					'frame' => $frame,
-// 					'dimensions' => $dimensions,
-// 					'petrol_cap' => $petrol_cap,
-// 					'f_tyre' => $f_tyre,
-// 					'b_tyre' => $b_tyre,
-// 					'dry_weight' => $dry_weight);
-				// header("Location: ../pages/new-bikes/bikecompare/comparetemp.php?$arraystring");			
--- pages/new-bikes/bikecompare/comparetemp.php
+++ pages/new-bikes/bikecompare/comparetemp.php
@@ -1,48 +1,169 @@
 <?php
 include_once '../../../includes/header.php';
+include_once '../../../includes/dbh.inc.php';
+
+
+$stmt = mysqli_stmt_init($conn);
+
+$bike1 = $_GET['bike1'];
+$bike2 = $_GET['bike2'];
+
+$sql = "SELECT * FROM bikespecs WHERE bike_id = {$bike1} OR bike_id = {$bike2};";
+
+if(!mysqli_stmt_prepare($stmt, $sql))
+{
+	echo "SQL statement failed";
+}
+else
+{
+	mysqli_stmt_execute($stmt);
+	$result1 = mysqli_stmt_get_result($stmt);
+
+	while($row1 = mysqli_fetch_assoc($result1)) 
+	{
+		if ($row1['bike_id'] == $bike1) {
+			$bike1eng = $row1['bike_engine_type'];
+			$bike1bore_str = $row1['bike_bore_stroke']; 
+			$bike1trans = $row1['bike_transmission']; 
+			$bike1starting = $row1['bike_starting']; 
+			$bike1frame = $row1['bike_frame']; 
+			$bike1dimensions = $row1['bike_dimensions']; 
+			$bike1petrol_cap = $row1['bike_petrol_cap']; 
+			$bike1f_tyre = $row1['bike_f_tyre']; 
+			$bike1b_tyre = $row1['bike_b_tyre']; 
+			$bike1dry_weight = $row1['bike_dry_weight']; 
+		}
+		if ($row1['bike_id'] == $bike2) {
+			$bike2eng = $row1['bike_engine_type'];
+			$bike2bore_str = $row1['bike_bore_stroke']; 
+			$bike2trans = $row1['bike_transmission']; 
+			$bike2starting = $row1['bike_starting']; 
+			$bike2frame = $row1['bike_frame']; 
+			$bike2dimensions = $row1['bike_dimensions']; 
+			$bike2petrol_cap = $row1['bike_petrol_cap']; 
+			$bike2f_tyre = $row1['bike_f_tyre']; 
+			$bike2b_tyre = $row1['bike_b_tyre']; 
+			$bike2dry_weight = $row1['bike_dry_weight']; 
+		}
+	}
+}
+$sql1 = "SELECT * FROM bikes WHERE bike_id = {$bike1} OR bike_id = {$bike2};";
+if(!mysqli_stmt_prepare($stmt, $sql1))
+{
+	echo "SQL statement failed";
+}
+else
+{
+	mysqli_stmt_execute($stmt);
+	$result3 = mysqli_stmt_get_result($stmt);
+
+	while($row3 = mysqli_fetch_assoc($result3)) 
+	{
+		if ($row3['bike_id'] == $bike1) 
+		{
+			$bike1Name = $row3['bike_brand'] . "" . $row3['bike_model'] . "" . $row3['bike_year'];
+		}
+		if ($row3['bike_id'] == $bike2) 
+		{
+			$bike2Name = $row3['bike_brand'] . "" . $row3['bike_model'] . "" . $row3['bike_year'];
+		}
+	}
+}
 ?>
 
 <section id="postad" class="section postadsection content">
 	<div class="container">
-		<?php
-		$bike1 = $_GET['bike1'];
-		$bike2 = $_GET['bike2'];
-		// print_r($_SESSION['bikespecs']);
-		foreach($_SESSION['bikespecs'] as $bike1 => $item)
-		{?>
-			<p><?php echo $item['eng_type']; ?></p>	
-			<p><?php echo $item['bore_str']; ?></p>	
-			<p><?php echo $item['trans']; ?></p>	
-			<p><?php echo $item['starting']; ?></p>	
-			<p><?php echo $item['frame']; ?></p>	
-			<p><?php echo $item['dimensions']; ?></p>	
-			<p><?php echo $item['petrol_cap']; ?></p>	
-			<p><?php echo $item['f_tyre']; ?></p>	
-			<p><?php echo $item['b_tyre']; ?></p>	
-			<p><?php echo $item['dry_weight']; ?></p>	
-			<?php
-			
-		}
-		foreach($_SESSION['bikespecs'] as $bike2 => $item)
-		{?>
-			<p><?php echo $item['eng_type']; ?></p>	
-			<p><?php echo $item['bore_str']; ?></p>	
-			<p><?php echo $item['trans']; ?></p>	
-			<p><?php echo $item['starting']; ?></p>	
-			<p><?php echo $item['frame']; ?></p>	
-			<p><?php echo $item['dimensions']; ?></p>	
-			<p><?php echo $item['petrol_cap']; ?></p>	
-			<p><?php echo $item['f_tyre']; ?></p>	
-			<p><?php echo $item['b_tyre']; ?></p>	
-			<p><?php echo $item['dry_weight']; ?></p>	
-			<?php
-			unset($_SESSION['bikespecs']);
-		}
-		?>
+		<table style="width: 100%">
+			<col style="width:30%" span="4" />
+			<tr>
+				<th></th>
+				<?php 
+				$sql2 = "SELECT * FROM b_images WHERE (bike_id = {$bike1} OR bike_id = {$bike2}) AND bike_image_thumb = '0';";
+				if(!mysqli_stmt_prepare($stmt, $sql2))
+				{
+					echo "SQL statement failed";
+				}
+				else
+				{
+					mysqli_stmt_execute($stmt);
+					$result2 = mysqli_stmt_get_result($stmt);
 
+					while($row2= mysqli_fetch_assoc($result2)) 
+					{
+						?>
+						<?php
+						if ($row2['bike_id'] == $bike1) 
+						{
+							?>
+							<td style="width: 30%;">
+								<img src="/BikeLabs/images/sparepartimg/<?php echo $row2['bike_image_name']; ?>"> 
+								<p><?php echo $row1['bike_image_name']; ?></p>
+							</td>
+							<?php 
+						}
+						if ($row2['bike_id'] == $bike2)
+						{
+							?>
+							<td style="width: 30%;"><img src="/BikeLabs/images/sparepartimg/<?php echo $row2['bike_image_name']; ?>"></td>
+							<?php 
+						}
+					}
+				}
+				?>
+			</tr>
+		</table>
+		<table style="width:100%" border="0">
+			<col style="width:20%" span="4" />
+			<tr>
+				<th>Engine Type</th>
+				<td><p><?php echo $bike1eng; ?></p></td>
+				<td><p><?php echo $bike2eng; ?></p></td>
+			</tr>
+			<tr>
+				<th>Bore & Stroke</th>
+				<td><p><?php echo $bike1bore_str; ?></p></td>
+				<td><p><?php echo $bike2bore_str; ?></p></td>
+			</tr>
+			<tr>
+				<th>Transmission</th>
+				<td><p><?php echo $bike1trans; ?></p></td>
+				<td><p><?php echo $bike2trans; ?></p></td>
+			</tr>
+			<tr>
+				<th>Starting</th>
+				<td><p><?php echo $bike1starting; ?></p></td>
+				<td><p><?php echo $bike2starting; ?></p></td>
+			</tr>
+			<tr>
+				<th>Frame</th>
+				<td><p><?php echo $bike1frame; ?></p></td>
+				<td><p><?php echo $bike2frame; ?></p></td>
+			</tr>
+			<tr>
+				<th>Dimension (Lxwxh)</th>
+				<td><p><?php echo $bike1dimensions; ?></p></td>
+				<td><p><?php echo $bike2dimensions; ?></p></td>
+			</tr>
+			<tr>
+				<th>Petrol Capacity</th>
+				<td><p><?php echo $bike1petrol_cap; ?></p></td>
+				<td><p><?php echo $bike2petrol_cap; ?></p></td>
+			</tr>
+			<tr>
+				<th>Tyre at Front</th>
+				<td><p><?php echo $bike1f_tyre; ?></p></td>
+				<td><p><?php echo $bike2f_tyre; ?></p></td>
+			</tr>
+			<tr>
+				<th>Tyre at Back</th>
+				<td><p><?php echo $bike1b_tyre; ?></p></td>
+				<td><p><?php echo $bike2b_tyre; ?></p></td>
+			</tr>
+			<tr>
+				<th>Dry Weight</th>
+				<td><p><?php echo $bike1dry_weight; ?></p></td>
+				<td><p><?php echo $bike2dry_weight; ?></p></td>
+			</tr>
+		</table>
 	</div>
 </section>
-
-<?php
-include '../../../includes/footer.php';
-?>
