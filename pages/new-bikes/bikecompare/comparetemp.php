9b23026a3de505340073b90a8fcaf66f0e1fefd2
Instabash <johnathan.lobo@gmail.com>
2019-05-23 12:33:34 +0500

    bike compare v1.2 made changes to styling

--- pages/new-bikes/bikecompare/comparetemp.php
+++ pages/new-bikes/bikecompare/comparetemp.php
@@ -61,11 +61,11 @@ else
 	{
 		if ($row3['bike_id'] == $bike1) 
 		{
-			$bike1Name = $row3['bike_brand'] . "" . $row3['bike_model'] . "" . $row3['bike_year'];
+			$bike1Name = $row3['bike_brand'] . " " . $row3['bike_model'] . " " . $row3['bikeyear'];
 		}
 		if ($row3['bike_id'] == $bike2) 
 		{
-			$bike2Name = $row3['bike_brand'] . "" . $row3['bike_model'] . "" . $row3['bike_year'];
+			$bike2Name = $row3['bike_brand'] . " " . $row3['bike_model'] . " " . $row3['bikeyear'];
 		}
 	}
 }
@@ -73,96 +73,104 @@ else
 
 <section id="postad" class="section postadsection content">
 	<div class="container">
-		<table style="width: 100%">
-			<col style="width:30%" span="4" />
-			<tr>
-				<th></th>
-				<?php 
-				$sql2 = "SELECT * FROM b_images WHERE (bike_id = {$bike1} OR bike_id = {$bike2}) AND bike_image_thumb = '0';";
-				if(!mysqli_stmt_prepare($stmt, $sql2))
-				{
-					echo "SQL statement failed";
-				}
-				else
-				{
-					mysqli_stmt_execute($stmt);
-					$result2 = mysqli_stmt_get_result($stmt);
-
-					while($row2= mysqli_fetch_assoc($result2)) 
+		<div style="padding-bottom: 50px;">
+			<h2 style=""><?php echo $bike1Name . " Vs. " . $bike2Name; ?></h2 >
+		</div>
+		<div style="background-color: #f2f3f3;border-radius: 4px;">
+			<table style="width: 100%;margin-bottom: 60px;">
+				<col style="width:30%" span="4" />
+				<tr>
+					<th></th>
+					<?php 
+					$sql2 = "SELECT * FROM b_images WHERE (bike_id = {$bike1} OR bike_id = {$bike2}) AND bike_image_thumb = '0';";
+					if(!mysqli_stmt_prepare($stmt, $sql2))
 					{
-						?>
-						<?php
-						if ($row2['bike_id'] == $bike1) 
+						echo "SQL statement failed";
+					}
+					else
+					{
+						mysqli_stmt_execute($stmt);
+						$result2 = mysqli_stmt_get_result($stmt);
+
+						while($row2= mysqli_fetch_assoc($result2)) 
 						{
 							?>
-							<td style="width: 30%;">
-								<img src="/BikeLabs/images/sparepartimg/<?php echo $row2['bike_image_name']; ?>"> 
-								<p><?php echo $row1['bike_image_name']; ?></p>
-							</td>
-							<?php 
-						}
-						if ($row2['bike_id'] == $bike2)
-						{
-							?>
-							<td style="width: 30%;"><img src="/BikeLabs/images/sparepartimg/<?php echo $row2['bike_image_name']; ?>"></td>
-							<?php 
+							<?php
+							if ($row2['bike_id'] == $bike1) 
+							{
+								?>
+								<td class="" style="width: 30%;padding: 30px;">
+									<img src="/BikeLabs/images/sparepartimg/<?php echo $row2['bike_image_name']; ?>" style="border:3px solid black;border-radius: 4px;"> <br><br>
+									<p style="text-align: center;"><?php echo $bike1Name; ?></p>
+								</td>
+								<?php 
+							}
+							if ($row2['bike_id'] == $bike2)
+							{
+								?>
+								<td class="" style="width: 30%;padding: 30px;">
+									<img src="/BikeLabs/images/sparepartimg/<?php echo $row2['bike_image_name']; ?>" style="border:3px solid black;border-radius: 4px;"> <br><br>
+									<p style="text-align: center;"><?php echo $bike2Name; ?></p>
+								</td>
+								<?php 
+							}
 						}
 					}
-				}
-				?>
-			</tr>
-		</table>
-		<table style="width:100%" border="0">
+					?>
+				</tr>
+			</table>
+		</div>
+		<table class="table" style="width:100%">
 			<col style="width:20%" span="4" />
 			<tr>
 				<th>Engine Type</th>
-				<td><p><?php echo $bike1eng; ?></p></td>
-				<td><p><?php echo $bike2eng; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike1eng; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike2eng; ?></p></td>
 			</tr>
 			<tr>
 				<th>Bore & Stroke</th>
-				<td><p><?php echo $bike1bore_str; ?></p></td>
-				<td><p><?php echo $bike2bore_str; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike1bore_str; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike2bore_str; ?></p></td>
 			</tr>
 			<tr>
 				<th>Transmission</th>
-				<td><p><?php echo $bike1trans; ?></p></td>
-				<td><p><?php echo $bike2trans; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike1trans; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike2trans; ?></p></td>
 			</tr>
 			<tr>
 				<th>Starting</th>
-				<td><p><?php echo $bike1starting; ?></p></td>
-				<td><p><?php echo $bike2starting; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike1starting; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike2starting; ?></p></td>
 			</tr>
 			<tr>
 				<th>Frame</th>
-				<td><p><?php echo $bike1frame; ?></p></td>
-				<td><p><?php echo $bike2frame; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike1frame; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike2frame; ?></p></td>
 			</tr>
 			<tr>
 				<th>Dimension (Lxwxh)</th>
-				<td><p><?php echo $bike1dimensions; ?></p></td>
-				<td><p><?php echo $bike2dimensions; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike1dimensions; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike2dimensions; ?></p></td>
 			</tr>
 			<tr>
 				<th>Petrol Capacity</th>
-				<td><p><?php echo $bike1petrol_cap; ?></p></td>
-				<td><p><?php echo $bike2petrol_cap; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike1petrol_cap; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike2petrol_cap; ?></p></td>
 			</tr>
 			<tr>
 				<th>Tyre at Front</th>
-				<td><p><?php echo $bike1f_tyre; ?></p></td>
-				<td><p><?php echo $bike2f_tyre; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike1f_tyre; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike2f_tyre; ?></p></td>
 			</tr>
 			<tr>
 				<th>Tyre at Back</th>
-				<td><p><?php echo $bike1b_tyre; ?></p></td>
-				<td><p><?php echo $bike2b_tyre; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike1b_tyre; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike2b_tyre; ?></p></td>
 			</tr>
 			<tr>
 				<th>Dry Weight</th>
-				<td><p><?php echo $bike1dry_weight; ?></p></td>
-				<td><p><?php echo $bike2dry_weight; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike1dry_weight; ?></p></td>
+				<td><p style="text-align: center;"><?php echo $bike2dry_weight; ?></p></td>
 			</tr>
 		</table>
 	</div>
