adf83500af1bacb64e22892135a3efd9e5fae8c7
Instabash <johnathan.lobo@gmail.com>
2019-05-28 00:11:30 +0500

    added account information to user dashboard

--- pages/user/userdash.php
+++ pages/user/userdash.php
@@ -2,16 +2,82 @@
 session_start();
 include '../../includes/restrictions.inc.php';
 user_protect();
-include_once '../../includes/header.php';
-include_once '../../includes/dbh.inc.php';
-
+include '../../includes/header.php';
+include '../../includes/dbh.inc.php';
+$user_id = $_SESSION['userId'];
 ?>
-
-<section id="biketemplate" class="section biketemplatesec content">
-	<div class="container" >
-		<a href="#">Go to chat</a>
+<label href="#" class="list-group-item" style="width: auto;">User Panel
+	<button class="btn" id="menu-toggle"><i class="fas fa-bars"></i></button>
+</label>
+<div class="d-flex" id="wrapper">
+	<div class="bg-light border-right" id="sidebar-wrapper">
+		<div class="list-group list-group-flush">
+			<a href="/BikeLabs/pages/user/userdash.php" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+			<a href="/BikeLabs/pages/user/user-ads.php" class="list-group-item list-group-item-action bg-light">Posted Adverts.</a>
+			<a href="/BikeLabs/pages/user/user-purchases.php" class="list-group-item list-group-item-action bg-light">Past Purchases</a>
+			<a href="/BikeLabs/pages/user/user-pending-orders.php" class="list-group-item list-group-item-action bg-light">Pending Orders</a>
+		</div>
 	</div>
-</section>
+
+	<!-- Main content -->
+	<section class="section modsection content content2" style="padding-left:5%;width: 100%;">
+		<div class="pb-5" >
+			<h3>User Panel</h3>
+		</div>
+		
+		<div>
+			<b><h4>Account Information</h4></b>
+		</div>
+
+		<div class="border border-new border-black p-5 rounded mt-3">
+			<?php		
+				$sqlacc = "SELECT * FROM users WHERE idUsers = ?";
+				$stmt = mysqli_stmt_init($conn);
+				
+
+				if (!mysqli_stmt_prepare($stmt, $sqlacc)) 
+				{
+					header("Location: userdash.php?error=sqlerror");
+					exit();	
+				}
+				else
+				{
+					mysqli_stmt_bind_param($stmt, "s", $user_id);
+					mysqli_stmt_execute($stmt);
+
+					$result = mysqli_stmt_get_result($stmt);
+					while($row1 = mysqli_fetch_assoc($result))
+					{
+
+					?>
+			
+			<!-- <div class="p-2">
+				<h5>First Name</h5>
+		 		<label><?php echo $row1['uidUsers']; ?></label>
+			</div>
+			<div class="p-2">
+				<h5>Last Name</h5>
+		 		<label><?php echo $row1['uidUsers']; ?></label>
+			</div> -->
+			<div class="p-2">
+				<h5>Display Name</h5>
+		 		<label><?php echo $row1['uidUsers']; ?></label>
+			</div>
+			<div class="p-2">
+				<h5>E-mail Address</h5>
+		 		<label><?php echo $row1['emailUsers']; } } ?></label>
+			</div>
+			<div class="p-2">
+				<h5>Password</h5>
+		 		<label>*********</label>
+			</div>
+			<div class="p-2" >
+				<a href="#" style="color: #dc3545 !important;">Edit Account</a>
+			</div>
+		 	
+		</div>
+	</section>	
+</div>
 
 <?php
 include_once '../../includes/footer.php';
