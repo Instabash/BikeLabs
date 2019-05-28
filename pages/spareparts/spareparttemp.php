f65132b448a3c9c572b7243ced7778405aad907f
Instabash <johnathan.lobo@gmail.com>
2019-05-28 17:51:07 +0500

    made minor changes to layout

--- pages/spareparts/spareparttemp.php
+++ pages/spareparts/spareparttemp.php
@@ -51,125 +51,253 @@ $stmt = mysqli_stmt_init($conn);
 						mysqli_stmt_execute($stmt);
 						$result1 = mysqli_stmt_get_result($stmt);
 						while ($row1 = mysqli_fetch_assoc($result1)) 
-							{ $i++;
-								?>
-								<div class="w3-col s4">
-									<img class="demo w3-opacity w3-hover-opacity-off border-new border border-dark rounded " src="../../images/sparepartimg/<?php echo $row1['ad_image_name'] ?>" style="width:100px;cursor:pointer" onclick="currentDiv(<?php echo $i ?>)">
-								</div>
+						{ 
+							$i++;
+							?>
+							<div class="w3-col s4">
+								<img class="demo w3-opacity w3-hover-opacity-off border-new border border-dark rounded " src="../../images/sparepartimg/<?php echo $row1['ad_image_name'] ?>" style="width:100px;cursor:pointer" onclick="currentDiv(<?php echo $i ?>)">
+							</div>
 
-								<?php 
+							<?php 
 
-							}
-						}		
-						?>
-					</div>
+						}
+					}		
+					?>
 				</div>
-				<div class="paymentright">
-					<div class="border-new border border-dark rounded mt-5 p-3" style="">
-						<?php
-						if($row = mysqli_fetch_assoc($result))
-						{?>
-							
-							<div>
-							<h5><b><?php echo $row['ad_title']; ?></b></h5><br>
+			</div>
+			<div class="paymentright">
+				<div class="border-new border border-dark rounded mt-5 p-3" style="">
+					<?php
+					if($row = mysqli_fetch_assoc($result))
+					{
+						?>
+						<div>
+							<h4><?php echo $row['ad_title']; ?></b></h4>
 							<p><?php echo $row['ad_price']; ?> Rs.</p>
 
 							<?php 
-								$sqladdesc = "SELECT * FROM users WHERE idUsers ={$row["idUsers"]}";
-								if(!mysqli_stmt_prepare($stmt, $sqladdesc))
-										{
-											echo "SQL statement failed";
-										}
-										else
-										{
-											mysqli_stmt_execute($stmt);
-											$result3 = mysqli_stmt_get_result($stmt);
-											while ($row3 = mysqli_fetch_assoc($result3)) 
-											{?>
-												<p>Posted by : <?php echo " " .  $row3['uidUsers']; ?></p>
-											<?php }
-										}
+							$sqladdesc = "SELECT * FROM users WHERE idUsers ={$row["idUsers"]}";
+							if(!mysqli_stmt_prepare($stmt, $sqladdesc))
+							{
+								echo "SQL statement failed";
+							}
+							else
+							{
+								mysqli_stmt_execute($stmt);
+								$result3 = mysqli_stmt_get_result($stmt);
+								while ($row3 = mysqli_fetch_assoc($result3)) 
+								{
+									?>
+									<p>Posted by : <?php echo " " .  $row3['uidUsers']; ?></p>
+									<?php 
+								}
+							}
 							?>
-							
-							</div>
+						</div>
 						<?php
 					}?>
-					</div>
-					<div class="border-new border border-dark rounded mt-5 p-3" style="">
-						<?php
-
-						echo '
-						<div>
-						<h5><b>Description</b></h5><br>	
-						<p>'.$row['ad_description'].'</p>
-						</div>
-						';
-						?>
-					</div>
-
-					<form action="../../includes/markadsold.inc.php?partid=$part_id" method="post">
-						
-						<div class="border-new border border-dark rounded mt-5 p-3">
+				</div>
+				<form action="../../includes/markadsold.inc.php?partid=$part_id" method="post">
+					<div class="border-new border border-dark rounded mt-5 p-3">
+						<?php 
+						if(isset($_SESSION['userId']))
+						{
+							if (($row['idUsers'] == $_SESSION['userId'])) 
+							{
+								?>
+								<button type="button" name="mksold" class="btn btn-outline-danger">Mark Ad. as sold</button>
 								<?php 
-								if(isset($_SESSION['userId']))
+							}
+							else
+							{
+								$sqladdesc = "SELECT * FROM users WHERE idUsers ={$row["idUsers"]}";
+								if(!mysqli_stmt_prepare($stmt, $sqladdesc))
 								{
-									if (($row['idUsers'] == $_SESSION['userId'])) 
-									{?>
-										<button type="button" name="mksold" class="btn btn-outline-danger">Mark Ad. as sold</button>
-									<?php 
-									}
-									else
-									{
-										$sqladdesc = "SELECT * FROM users WHERE idUsers ={$row["idUsers"]}";
-										if(!mysqli_stmt_prepare($stmt, $sqladdesc))
-										{
-											echo "SQL statement failed";
-										}
-										else
-										{
-											mysqli_stmt_execute($stmt);
-											$result2 = mysqli_stmt_get_result($stmt);
-											while ($row2 = mysqli_fetch_assoc($result2)) 
-												{?>
-										<p>Contact this user at : <?php echo $row2['emailUsers'];?></p>
-										<p>Or</p>
-										<p>at : <?php echo $row2['User_Contact'];?></p>
-									<?php
-											}
-										} 
-									}
+									echo "SQL statement failed";
 								}
 								else
 								{
-									$sqladdesc = "SELECT * FROM users WHERE idUsers ={$row["idUsers"]}";
-									if(!mysqli_stmt_prepare($stmt, $sqladdesc))
+									mysqli_stmt_execute($stmt);
+									$result2 = mysqli_stmt_get_result($stmt);
+									while ($row2 = mysqli_fetch_assoc($result2)) 
 									{
-										echo "SQL statement failed";
-									}
-									else
-									{
-										mysqli_stmt_execute($stmt);
-										$result2 = mysqli_stmt_get_result($stmt);
-										while ($row2 = mysqli_fetch_assoc($result2)) 
-											{?>
-									
-										<h5><b>User Contact</b></h5><br>
-										<p><b>Email:</b></p>
-										<p><?php echo $row2['emailUsers'];?></p>
+										?>
+										<p>Contact this user at : <?php echo $row2['emailUsers'];?></p>
 										<p>Or</p>
-										<p><b>Phone:</b></p>
 										<p>at : <?php echo $row2['User_Contact'];?></p>
+										<?php
+									}
+								} 
+							}
+						}
+						else
+						{
+							$sqladdesc = "SELECT * FROM users WHERE idUsers ={$row["idUsers"]}";
+							if(!mysqli_stmt_prepare($stmt, $sqladdesc))
+							{
+								echo "SQL statement failed";
+							}
+							else
+							{
+								mysqli_stmt_execute($stmt);
+								$result2 = mysqli_stmt_get_result($stmt);
+								while ($row2 = mysqli_fetch_assoc($result2)) 
+								{
+									?>
+
+									<h5><b>User Contact</b></h5><br>
+									<p><b>Email:</b></p>
+									<p><?php echo $row2['emailUsers'];?></p>
+									<p>Or</p>
+									<p><b>Phone:</b></p>
+									<p>at : <?php echo $row2['User_Contact'];?></p>
 
 									<?php
-										}
-									} 
 								}
-								?>
+							} 
+						}
+						?>
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="paymentmain" style="margin-left: 0px;margin-right: 0px">
+			<div class="paymentleft" >
+				<div class="border-new border border-dark rounded mt-5 p-3">
+					<h4>Description</h4><br>	
+					<p><?php echo $row['ad_description']; ?></p>
+				</div>
+				<?php
+				
+				$sqlspecs = "SELECT * FROM bikespecs WHERE bike_id = {$bike_id};";
+				if(!mysqli_stmt_prepare($stmt, $sqlspecs))
+				{
+					echo "SQL statement failed";
+				}
+				else
+				{
+					mysqli_stmt_execute($stmt);
+					$result1 = mysqli_stmt_get_result($stmt);
+
+					if($row1 = mysqli_fetch_assoc($result1)) 
+					{
+						$bike1eng = $row1['bike_engine_type'];
+						$bike1bore_str = $row1['bike_bore_stroke']; 
+						$bike1trans = $row1['bike_transmission']; 
+						$bike1starting = $row1['bike_starting']; 
+						$bike1frame = $row1['bike_frame']; 
+						$bike1dimensions = $row1['bike_dimensions']; 
+						$bike1petrol_cap = $row1['bike_petrol_cap']; 
+						$bike1f_tyre = $row1['bike_f_tyre']; 
+						$bike1b_tyre = $row1['bike_b_tyre']; 
+						$bike1dry_weight = $row1['bike_dry_weight']; 
+					}?>
+					<div class="border-new border border-dark rounded mt-5 p-3">
+						<h4>Bike Specifications</h4><br>
+						<table class="table" style="width:100%">
+							<col style="width:20%" span="4" />
+							<tr>
+								<th>Engine Type</th>
+								<td><p style="text-align: center;"><?php echo $bike1eng; ?></p></td>
+							</tr>
+							<tr>
+								<th>Bore & Stroke</th>
+								<td><p style="text-align: center;"><?php echo $bike1bore_str; ?></p></td>
+							</tr>
+							<tr>
+								<th>Transmission</th>
+								<td><p style="text-align: center;"><?php echo $bike1trans; ?></p></td>
+							</tr>
+							<tr>
+								<th>Starting</th>
+								<td><p style="text-align: center;"><?php echo $bike1starting; ?></p></td>
+							</tr>
+							<tr>
+								<th>Frame</th>
+								<td><p style="text-align: center;"><?php echo $bike1frame; ?></p></td>
+							</tr>
+							<tr>
+								<th>Dimension (Lxwxh)</th>
+								<td><p style="text-align: center;"><?php echo $bike1dimensions; ?></p></td>
+							</tr>
+							<tr>
+								<th>Petrol Capacity</th>
+								<td><p style="text-align: center;"><?php echo $bike1petrol_cap; ?></p></td>
+							</tr>
+							<tr>
+								<th>Tyre at Front</th>
+								<td><p style="text-align: center;"><?php echo $bike1f_tyre; ?></p></td>
+							</tr>
+							<tr>
+								<th>Tyre at Back</th>
+								<td><p style="text-align: center;"><?php echo $bike1b_tyre; ?></p></td>
+							</tr>
+							<tr>
+								<th>Dry Weight</th>
+								<td><p style="text-align: center;"><?php echo $bike1dry_weight; ?></p></td>
+							</tr>
+						</table>
+					</div>
+					<div class="border-new border border-dark rounded mt-5 p-3">
+						<?php
+						$bikeinfosql = "SELECT * FROM bikes WHERE bike_id = {$bike_id};";
+						if(!mysqli_stmt_prepare($stmt, $bikeinfosql))
+						{
+							echo "SQL statement failed";
+						}
+						else
+						{
+							mysqli_stmt_execute($stmt);
+							$result1 = mysqli_stmt_get_result($stmt);
+
+							if($row2 = mysqli_fetch_assoc($result1)) 
+							{
+								$bike_name = $row2['bike_brand'] . " " . $row2['bike_model'] . " " . $row2['bikeyear'];
+								$bike1_model = $row2['bike_model'];
+								$bike1_make = $row2['bike_brand'];
+							}
+						}
+						?>
+						<h5>Compare <b><?php echo $bike_name; ?></b> with other bikes</h5>
+						<form action="../../includes/bikecompare.inc.php" method="post">
+							<div class="row pt-5 pl-5 pr-5">
+								<div class="modleft1">
+									<h5>Choose Bike</h5>
+									<div id="demoForm" class="demoForm p-4">
+										<div class="formrowad p-2">
+											<label>Make</label>
+											<div class="select-wrap mb-2">
+												<select class="custom-select" name="category" id="category">
+													<option value="Honda">Honda</option>
+													<option value="SuperPower">SuperPower</option>
+													<option value="Unique">Unique</option> 
+												</select>
+											</div>
+										</div>
+										<div class="formrowad p-2">
+											<label>Make</label>
+											<div class="select-wrap mb-2">
+												<select class="custom-select" name="choices" id="choices">
+													<!-- populated using JavaScript -->
+												</select>
+											</div>
+										</div>
+										<div>
+											<input type="hidden" name="category2" value="<?php echo $bike1_make ?>">
+											<input type="hidden" name="choices2" value="<?php echo $bike1_model ?>">
+										</div>
+									</div>
+								</div>
 							</div>
+							<input type="submit" class="btn btn-outline-danger" name="cmpsubmit" value="Compare">
 						</form>
 					</div>
-				</div>
+					<?php
+				}
+				?>
 			</div>
+		</div>
+	</div>
 </section>
 <script type="text/javascript" src="../../script/getparameters.js"></script>
 <script>
@@ -177,76 +305,76 @@ $stmt = mysqli_stmt_init($conn);
 	showDivs(slideIndex);
 
 	function plusDivs(n) {
-	showDivs(slideIndex += n);
+		showDivs(slideIndex += n);
 	}
 
 	function showDivs(n) {
-	var i;
-	var x = document.getElementsByClassName("mySlides");
-	if (n > x.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = x.length}
-	for (i = 0; i < x.length; i++) {
-	x[i].style.display = "none";  
-	}
-	x[slideIndex-1].style.display = "block";  
-	}
-	$('.btn-number').click(function(e){
-	e.preventDefault();
+		var i;
+		var x = document.getElementsByClassName("mySlides");
+		if (n > x.length) {slideIndex = 1}
+			if (n < 1) {slideIndex = x.length}
+				for (i = 0; i < x.length; i++) {
+					x[i].style.display = "none";  
+				}
+				x[slideIndex-1].style.display = "block";  
+			}
+			$('.btn-number').click(function(e){
+				e.preventDefault();
 
-	fieldName = $(this).attr('data-field');
-	type      = $(this).attr('data-type');
-	var input = $("input[name='"+fieldName+"']");
-	var currentVal = parseInt(input.val());
-	if (!isNaN(currentVal)) {
-	if(type == 'minus') {
+				fieldName = $(this).attr('data-field');
+				type      = $(this).attr('data-type');
+				var input = $("input[name='"+fieldName+"']");
+				var currentVal = parseInt(input.val());
+				if (!isNaN(currentVal)) {
+					if(type == 'minus') {
 
-	if(currentVal > input.attr('min')) {
-	input.val(currentVal - 1).change();
-	} 
-	if(parseInt(input.val()) == input.attr('min')) {
-	$(this).attr('disabled', true);
-	}
+						if(currentVal > input.attr('min')) {
+							input.val(currentVal - 1).change();
+						} 
+						if(parseInt(input.val()) == input.attr('min')) {
+							$(this).attr('disabled', true);
+						}
 
-	} else if(type == 'plus') {
+					} else if(type == 'plus') {
 
-	if(currentVal < input.attr('max')) {
-	input.val(currentVal + 1).change();
-	}
-	if(parseInt(input.val()) == input.attr('max')) {
-	$(this).attr('disabled', true);
-	}
+						if(currentVal < input.attr('max')) {
+							input.val(currentVal + 1).change();
+						}
+						if(parseInt(input.val()) == input.attr('max')) {
+							$(this).attr('disabled', true);
+						}
 
-	}
-	} else {
-	input.val(0);
-	}
-	});
-	$('.input-number').focusin(function(){
-	$(this).data('oldValue', $(this).val());
-	});
-	$('.input-number').change(function() {
+					}
+				} else {
+					input.val(0);
+				}
+			});
+			$('.input-number').focusin(function(){
+				$(this).data('oldValue', $(this).val());
+			});
+			$('.input-number').change(function() {
 
-	minValue =  parseInt($(this).attr('min'));
-	maxValue =  parseInt($(this).attr('max'));
-	valueCurrent = parseInt($(this).val());
+				minValue =  parseInt($(this).attr('min'));
+				maxValue =  parseInt($(this).attr('max'));
+				valueCurrent = parseInt($(this).val());
 
-	name = $(this).attr('name');
-	if(valueCurrent >= minValue) {
-	$(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
-	} else {
-	alert('Sorry, the minimum value was reached');
-	$(this).val($(this).data('oldValue'));
-	}
-	if(valueCurrent <= maxValue) {
-	$(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
-	} else {
-	alert('Sorry, the maximum value was reached');
-	$(this).val($(this).data('oldValue'));
-	}
+				name = $(this).attr('name');
+				if(valueCurrent >= minValue) {
+					$(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+				} else {
+					alert('Sorry, the minimum value was reached');
+					$(this).val($(this).data('oldValue'));
+				}
+				if(valueCurrent <= maxValue) {
+					$(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+				} else {
+					alert('Sorry, the maximum value was reached');
+					$(this).val($(this).data('oldValue'));
+				}
 
 
-	});
-	$(".input-number").keydown(function (e) {
+			});
+			$(".input-number").keydown(function (e) {
 	// Allow: backspace, delete, tab, escape, enter and .
 	if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
 	// Allow: Ctrl+A
@@ -254,16 +382,16 @@ $stmt = mysqli_stmt_init($conn);
 	// Allow: home, end, left, right
 	(e.keyCode >= 35 && e.keyCode <= 39)) {
 	// let it happen, don't do anything
-	return;
-	}
+return;
+}
 	// Ensure that it is a number and stop the keypress
 	if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-	e.preventDefault();
+		e.preventDefault();
 	}
-	});
+});
 
-	var query = window.location.search.substring(1);
-	var qs = parse_query_string(query);
+			var query = window.location.search.substring(1);
+			var qs = parse_query_string(query);
 	// console.log(qs.quant);	
 	var partid = qs.partid;
 	localStorage.setItem('partid', partid);
