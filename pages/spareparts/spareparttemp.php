fa06a11e8a6dde4aa2d1274dd82bfba02f40e973
Instabash <johnathan.lobo@gmail.com>
2019-05-29 23:23:42 +0500

    changed layout of page.

--- pages/spareparts/spareparttemp.php
+++ pages/spareparts/spareparttemp.php
@@ -72,33 +72,34 @@ $stmt = mysqli_stmt_init($conn);
 					{
 						?>
 						<div>
-							<h4><?php echo $row['ad_title']; ?></b></h4>
-							<p><?php echo $row['ad_price']; ?> Rs.</p>
-
-							<?php 
-							$sqladdesc = "SELECT * FROM users WHERE idUsers ={$row["idUsers"]}";
-							if(!mysqli_stmt_prepare($stmt, $sqladdesc))
-							{
-								echo "SQL statement failed";
-							}
-							else
-							{
-								mysqli_stmt_execute($stmt);
-								$result3 = mysqli_stmt_get_result($stmt);
-								while ($row3 = mysqli_fetch_assoc($result3)) 
-								{
-									?>
-									<p>Posted by : <?php echo " " .  $row3['uidUsers']; ?></p>
-									<?php 
-								}
-							}
-							?>
+							<!-- <h4><?php echo $row['ad_title']; ?></b></h4> -->
+							<h3><b><?php echo $row['ad_price']; ?></b> Rs.</h3>
 						</div>
 						<?php
 					}?>
 				</div>
+				<div class="border-new border border-dark rounded mt-2 p-3">
+					<?php 
+					$sqladdesc = "SELECT * FROM users WHERE idUsers ={$row["idUsers"]}";
+					if(!mysqli_stmt_prepare($stmt, $sqladdesc))
+					{
+						echo "SQL statement failed";
+					}
+					else
+					{
+						mysqli_stmt_execute($stmt);
+						$result3 = mysqli_stmt_get_result($stmt);
+						while ($row3 = mysqli_fetch_assoc($result3)) 
+						{
+							?>
+							<h5><b>Posted by :</b> <?php echo " " .  $row3['uidUsers']; ?></h5>
+							<?php 
+						}
+					}
+					?>
+				</div>
 				<form action="../../includes/markadsold.inc.php?partid=$part_id" method="post">
-					<div class="border-new border border-dark rounded mt-5 p-3">
+					<div class="border-new border border-dark rounded mt-2 p-3">
 						<?php 
 						if(isset($_SESSION['userId']))
 						{
@@ -122,9 +123,12 @@ $stmt = mysqli_stmt_init($conn);
 									while ($row2 = mysqli_fetch_assoc($result2)) 
 									{
 										?>
-										<p>Contact this user at : <?php echo $row2['emailUsers'];?></p>
+										<h5><b>User Contact</b></h5><br>
+										<p><b>Email:</b></p>
+										<p><?php echo $row2['emailUsers'];?></p>
 										<p>Or</p>
-										<p>at : <?php echo $row2['User_Contact'];?></p>
+										<p><b>Phone:</b></p>
+										<p><?php echo $row2['User_Contact'];?></p>
 										<?php
 									}
 								} 
@@ -144,14 +148,12 @@ $stmt = mysqli_stmt_init($conn);
 								while ($row2 = mysqli_fetch_assoc($result2)) 
 								{
 									?>
-
 									<h5><b>User Contact</b></h5><br>
 									<p><b>Email:</b></p>
 									<p><?php echo $row2['emailUsers'];?></p>
 									<p>Or</p>
 									<p><b>Phone:</b></p>
-									<p>at : <?php echo $row2['User_Contact'];?></p>
-
+									<p><?php echo $row2['User_Contact'];?></p>
 									<?php
 								}
 							} 
@@ -159,142 +161,28 @@ $stmt = mysqli_stmt_init($conn);
 						?>
 					</div>
 				</form>
+				<div class="border-new border border-dark rounded mt-2 p-3">
+					<h5><b>Ad. Location</b></h5><br>
+					<p>
+					<?php
+						echo $row['ad_user_hname'] . ", " . $row['ad_user_pcode'] . ", " . $row['ad_user_country'] ;
+					?></p>
+				</div>
+				<div class="border-new border border-dark rounded mt-2 p-3">
+					<h5><b>Date Posted</b></h5><br>
+					<p>
+					<?php
+						echo $row['ad_date'];
+					?></p>
+				</div>
 			</div>
 		</div>
 		<div class="paymentmain" style="margin-left: 0px;margin-right: 0px">
 			<div class="paymentleft" >
 				<div class="border-new border border-dark rounded mt-5 p-3">
-					<h4>Description</h4><br>	
+					<h4><b>Description</b></h4><br>	
 					<p><?php echo $row['ad_description']; ?></p>
 				</div>
-				<?php
-				
-				$sqlspecs = "SELECT * FROM bikespecs WHERE bike_id = {$bike_id};";
-				if(!mysqli_stmt_prepare($stmt, $sqlspecs))
-				{
-					echo "SQL statement failed";
-				}
-				else
-				{
-					mysqli_stmt_execute($stmt);
-					$result1 = mysqli_stmt_get_result($stmt);
-
-					if($row1 = mysqli_fetch_assoc($result1)) 
-					{
-						$bike1eng = $row1['bike_engine_type'];
-						$bike1bore_str = $row1['bike_bore_stroke']; 
-						$bike1trans = $row1['bike_transmission']; 
-						$bike1starting = $row1['bike_starting']; 
-						$bike1frame = $row1['bike_frame']; 
-						$bike1dimensions = $row1['bike_dimensions']; 
-						$bike1petrol_cap = $row1['bike_petrol_cap']; 
-						$bike1f_tyre = $row1['bike_f_tyre']; 
-						$bike1b_tyre = $row1['bike_b_tyre']; 
-						$bike1dry_weight = $row1['bike_dry_weight']; 
-					}?>
-					<div class="border-new border border-dark rounded mt-5 p-3">
-						<h4>Bike Specifications</h4><br>
-						<table class="table" style="width:100%">
-							<col style="width:20%" span="4" />
-							<tr>
-								<th>Engine Type</th>
-								<td><p style="text-align: center;"><?php echo $bike1eng; ?></p></td>
-							</tr>
-							<tr>
-								<th>Bore & Stroke</th>
-								<td><p style="text-align: center;"><?php echo $bike1bore_str; ?></p></td>
-							</tr>
-							<tr>
-								<th>Transmission</th>
-								<td><p style="text-align: center;"><?php echo $bike1trans; ?></p></td>
-							</tr>
-							<tr>
-								<th>Starting</th>
-								<td><p style="text-align: center;"><?php echo $bike1starting; ?></p></td>
-							</tr>
-							<tr>
-								<th>Frame</th>
-								<td><p style="text-align: center;"><?php echo $bike1frame; ?></p></td>
-							</tr>
-							<tr>
-								<th>Dimension (Lxwxh)</th>
-								<td><p style="text-align: center;"><?php echo $bike1dimensions; ?></p></td>
-							</tr>
-							<tr>
-								<th>Petrol Capacity</th>
-								<td><p style="text-align: center;"><?php echo $bike1petrol_cap; ?></p></td>
-							</tr>
-							<tr>
-								<th>Tyre at Front</th>
-								<td><p style="text-align: center;"><?php echo $bike1f_tyre; ?></p></td>
-							</tr>
-							<tr>
-								<th>Tyre at Back</th>
-								<td><p style="text-align: center;"><?php echo $bike1b_tyre; ?></p></td>
-							</tr>
-							<tr>
-								<th>Dry Weight</th>
-								<td><p style="text-align: center;"><?php echo $bike1dry_weight; ?></p></td>
-							</tr>
-						</table>
-					</div>
-					<div class="border-new border border-dark rounded mt-5 p-3">
-						<?php
-						$bikeinfosql = "SELECT * FROM bikes WHERE bike_id = {$bike_id};";
-						if(!mysqli_stmt_prepare($stmt, $bikeinfosql))
-						{
-							echo "SQL statement failed";
-						}
-						else
-						{
-							mysqli_stmt_execute($stmt);
-							$result1 = mysqli_stmt_get_result($stmt);
-
-							if($row2 = mysqli_fetch_assoc($result1)) 
-							{
-								$bike_name = $row2['bike_brand'] . " " . $row2['bike_model'] . " " . $row2['bikeyear'];
-								$bike1_model = $row2['bike_model'];
-								$bike1_make = $row2['bike_brand'];
-							}
-						}
-						?>
-						<h5>Compare <b><?php echo $bike_name; ?></b> with other bikes</h5>
-						<form action="../../includes/bikecompare.inc.php" method="post">
-							<div class="row pt-5 pl-5 pr-5">
-								<div class="modleft1">
-									<h5>Choose Bike</h5>
-									<div id="demoForm" class="demoForm p-4">
-										<div class="formrowad p-2">
-											<label>Make</label>
-											<div class="select-wrap mb-2">
-												<select class="custom-select" name="category" id="category">
-													<option value="Honda">Honda</option>
-													<option value="SuperPower">SuperPower</option>
-													<option value="Unique">Unique</option> 
-												</select>
-											</div>
-										</div>
-										<div class="formrowad p-2">
-											<label>Make</label>
-											<div class="select-wrap mb-2">
-												<select class="custom-select" name="choices" id="choices">
-													<!-- populated using JavaScript -->
-												</select>
-											</div>
-										</div>
-										<div>
-											<input type="hidden" name="category2" value="<?php echo $bike1_make ?>">
-											<input type="hidden" name="choices2" value="<?php echo $bike1_model ?>">
-										</div>
-									</div>
-								</div>
-							</div>
-							<input type="submit" class="btn btn-outline-danger" name="cmpsubmit" value="Compare">
-						</form>
-					</div>
-					<?php
-				}
-				?>
 			</div>
 		</div>
 	</div>
