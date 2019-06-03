28722d075404e73f39a20f22743e6a3745f0c52f
Instabash <johnathan.lobo@gmail.com>
2019-06-03 19:34:14 +0500

    added script for input masking

--- includes/header.php
+++ includes/header.php
@@ -80,6 +80,8 @@ if(!isset($_SESSION))
 	<!-- Datatables js -->
 	<script src="/BikeLabs/script/jquery.dataTables.min.js"></script>
 	<script src="/BikeLabs/script/dataTables.bootstrap.js"></script>
+	<script src="/BikeLabs/script/jquery.inputmask.js"></script>
+	<script src="/BikeLabs/script/inputmask.binding.js"></script>
 	<!---------------------------------Scripts end---------------------------------->
 
 </head>
