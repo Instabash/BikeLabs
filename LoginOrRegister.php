945e55f12f51f4da54a714fc0fb72869fa75c5c6
Instabash <johnathan.lobo@gmail.com>
2019-06-06 13:58:19 +0500

    Made some style changes to this form.

--- LoginOrRegister.php
+++ LoginOrRegister.php
@@ -4,20 +4,20 @@ include_once 'includes/header.php';
 
 <section id="signup" class="section fontsec content">
 	<div class="container">
-		<h3 class="p-4">Login</h3>
-		<div class="border border-dark border-new rounded pt-5">
-			<form class="" action="includes/login.inc.php" method="post">
+		<h1 class="p-4">Login</h1>
+		<div class="mt-4 border border-new border-black p-4" style="box-shadow: 6px 6px #e6e6e6;border-color: #cad1d8 !important;">
+			<form action="includes/login.inc.php" method="post">
 				<div class="form-group modmarginleft">
 					<div class="" >
-						<input type="text" class="form-control" name="mailuid" placeholder="Username/E-mail..">
+						<input type="text" class="form-control logintext" name="mailuid" placeholder="Username/E-mail..">
 					</div>
 				</div>
 				<div class="form-group modmarginleft">
 					<div class="" >
-						<input type="password" class="form-control" name="pwd" placeholder="Password..">
+						<input type="password" class="form-control logintext" name="pwd" placeholder="Password..">
 					</div>
 				</div>
-				<button type="submit" class="btn btn-outline-danger" name="login-submit">Login</button>
+				<button type="submit" class="loginbtn" name="login-submit">Login</button>
 			</form>
 			<p class="p-4">Or <a href="signup.php">register</a> if you are a new user.</p>
 		</div>
