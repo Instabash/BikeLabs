d0c7e039a8c9b9572f205e6f38ed868749fbfb36
Instabash <johnathan.lobo@gmail.com>
2019-06-07 22:26:27 +0500

    added model to filter url

--- includes/search.inc.php
+++ includes/search.inc.php
@@ -4,6 +4,21 @@ if(isset($_POST['search-sub']))
 {
 	$fromval = $_POST['fromval'];
 	$toval = $_POST['toval'];
+	$array = array();
 
-	header("Location: ../newbikes.php?pricefrom=$fromval&priceto=$toval");
+	if(!empty($_POST['modelcheck'])){
+		foreach($_POST['modelcheck'] as $selected){
+			$array[] = $selected;
+		}
+	}
+	$args = array (
+    'pricefrom' => $fromval,
+    'priceto' => $toval,
+    'model' => $array
+);
+
+		$query = preg_replace('/%5B(?:[0-9]|[1-9][0-9]+)%5D=/', '[]=', http_build_query($args));
+		// $cleanedParams = preg_replace('/%5B(\d+?)%5D/', '', $params);
+
+	header("Location: ../newbikes.php?" . $query);
 }
