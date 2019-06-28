221e34910327237b9f27f3a624f33e27b4ac70d7
Instabash <johnathan.lobo@gmail.com>
2019-06-28 10:25:07 +0500

    Mark ad as sold and remove bikes and parts functionality added

--- includes/markadsold.inc.php
+++ includes/markadsold.inc.php
@@ -1,9 +1,9 @@
 <?php
 include 'dbh.inc.php';
 if (isset($_POST['mksold'])) {
-	$part_id = $_GET['partid'];
+	$part_id = $_GET['ad_id'];
 
-	$sql = "DELETE FROM post_ad WHERE ad_id = ?;";
+	$sql = "DELETE FROM post_ad_images WHERE ad_id = ?;";
 	$stmt = mysqli_stmt_init($conn);
 
 	if (!mysqli_stmt_prepare($stmt, $sql)) 
@@ -14,7 +14,93 @@ if (isset($_POST['mksold'])) {
 	{	
 		mysqli_stmt_bind_param($stmt, "s", $part_id);
 		mysqli_stmt_execute($stmt);
+		$sql = "DELETE FROM post_ad WHERE ad_id = ?;";
+		$stmt = mysqli_stmt_init($conn);
 
-		$sqlimg = "DELETE FROM post_ad_images WHERE "
+		if (!mysqli_stmt_prepare($stmt, $sql)) 
+		{
+			echo "SQL statement failed";
+		}	
+		else
+		{	
+			mysqli_stmt_bind_param($stmt, "s", $part_id);
+			mysqli_stmt_execute($stmt);
+			header("Location: ../pages/user/user-ads.php?success");
+		}
+	}
+}
+elseif (isset($_POST['remove-bike'])) {
+	$part_id = $_GET['part_id'];
+	$sqlimg = "DELETE FROM b_images WHERE bike_id = ?;";
+	
+	$stmt = mysqli_stmt_init($conn);
+
+	if (!mysqli_stmt_prepare($stmt, $sqlimg)) 
+	{
+		header("Location: ../pages/admin/admin-bike-parts.php?error=sqlerror");
+		exit();
+	}
+	else
+	{	
+		mysqli_stmt_bind_param($stmt, "s", $part_id);
+		mysqli_stmt_execute($stmt);
+
+		$sql = "DELETE FROM bikespecs WHERE bike_id = ?;";
+		$stmt = mysqli_stmt_init($conn);
+
+		if (!mysqli_stmt_prepare($stmt, $sql)) 
+		{
+			echo "SQL statement failed";
+		}	
+		else
+		{	
+			mysqli_stmt_bind_param($stmt, "s", $part_id);
+			mysqli_stmt_execute($stmt);
+			$sql = "DELETE FROM bikes WHERE bike_id = ?;";
+			$stmt = mysqli_stmt_init($conn);
+
+			if (!mysqli_stmt_prepare($stmt, $sql)) 
+			{
+				echo "SQL statement failed";
+			}	
+			else
+			{	
+				mysqli_stmt_bind_param($stmt, "s", $part_id);
+				mysqli_stmt_execute($stmt);
+				header("Location: ../pages/admin/admin-bike-parts.php?success");
+			}
+			
+		}
+	}
+}
+elseif (isset($_POST['remove-part'])) {
+	$part_id = $_GET['part_id'];
+	$sqlimg = "DELETE FROM spare_part_images WHERE part_id = ?;";
+	
+	$stmt = mysqli_stmt_init($conn);
+
+	if (!mysqli_stmt_prepare($stmt, $sqlimg)) 
+	{
+		header("Location: ../pages/admin/admin-bike-parts.php?error=sqlerror");
+		exit();
+	}
+	else
+	{	
+		mysqli_stmt_bind_param($stmt, "s", $part_id);
+		mysqli_stmt_execute($stmt);
+
+		$sql = "DELETE FROM spare_parts WHERE part_id = ?;";
+		$stmt = mysqli_stmt_init($conn);
+
+		if (!mysqli_stmt_prepare($stmt, $sql)) 
+		{
+			echo "SQL statement failed";
+		}	
+		else
+		{	
+			mysqli_stmt_bind_param($stmt, "s", $part_id);
+			mysqli_stmt_execute($stmt);
+			header("Location: ../pages/admin/admin-bike-parts.php?success");
+		}
 	}
 }
