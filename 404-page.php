534fc5034ce1a6fd845d61304b8204a221d3ca52
Instabash <johnathan.lobo@gmail.com>
2019-06-30 16:29:01 +0500

    added a 404 page

---
+++ 404-page.php
@@ -0,0 +1,15 @@
+<?php
+	include_once 'includes/header.php';
+?>
+<section id="modify" class="section content">
+	<div class="container">
+		<h2 class="section-head">
+			<h1 class="">404 Page not found</h1>
+		</h2><br>
+		<p class="lead">The page you were looking for does not exist! Click <a href="../index.php">here</a> to return to the Home page. <br>If you got to this page by clicking a link, please email us so we can fix this issue.</p>
+		
+	</div>
+</section>
+<?php
+	include_once 'includes/footer.php';
+?>
