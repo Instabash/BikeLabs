88b1ef84640a172a5d769ec83cf915ac26fd574e
Instabash <johnathan.lobo@gmail.com>
2019-07-03 23:04:39 +0500

    added chat table and button for admin and vendor

--- pages/admin/admindash.php
+++ pages/admin/admindash.php
@@ -99,7 +99,7 @@ include '../../includes/dbh.inc.php';
 					</div>
 					<!-- /.box-body -->
 					<div class="box-footer clearfix">
-						<a href="/BikeLabs/pages/admin/admin-orders.php" class="btn btn-sm btn-default btn-flat pull-right">View All Orders</a>
+						<a href="/BikeLabs/pages/admin/admin-orders.php" class="btn btn-sm btn-primary pull-right">View All Orders</a>
 					</div>
 					<!-- /.box-footer -->
 				</div>
@@ -158,7 +158,7 @@ include '../../includes/dbh.inc.php';
 					</div>
 					<!-- /.box-body -->
 					<div class="box-footer clearfix">
-						<a href="#" class="btn btn-sm btn-default btn-flat pull-right">View All Users</a>
+						<a href="#" class="btn btn-sm btn-primary pull-right">View All Users</a>
 					</div>
 					<!-- /.box-footer -->
 				</div>
@@ -166,7 +166,64 @@ include '../../includes/dbh.inc.php';
 			</div>
 			<!-- /.col -->
 
+			<div class="col-sm-6 pt-5">
+				<div class="box box-info">
+					<div class="box-header with-border">
+						<h3 class="box-title">Latest messages</h3>
+					</div>
+					<!-- /.box-header -->
+					<div class="box-body">
+						<div class="table-responsive">
+							<table id="example1" class="table table-bordered table-striped">
+								<thead>
+									<tr>
+										<th>Sender</th>
+										<th>Time</th>
+										<th>Message</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php
+									$receiver = $_SESSION['userUId'];
+									$sql = "SELECT * FROM chat_message WHERE receiver_name = '$receiver';";
+									$stmt = mysqli_stmt_init($conn);
+									if (!mysqli_stmt_prepare($stmt, $sql)) 
+									{
+										echo("sql error");
+										echo mysqli_error($conn);
+										exit();
+									}
+									else
+									{
+										mysqli_stmt_execute($stmt);
+										$result = mysqli_stmt_get_result($stmt);
+										while($row = mysqli_fetch_assoc($result))
+										{
+											
+											?>
+											<tr>
+												<td><span class="label label-success"><?php echo $row['sender_name']; ?></span></td>
+												<td><?php echo $row['time_stamp']; ?></td>
+												<td><span class="label label-success"><?php echo $row['chat_message']; ?></span></td>
+											</tr>
+											<?php
 
+										}
+									}
+									?>
+								</tbody>
+							</table>
+						</div>
+						<!-- /.table-responsive -->
+					</div>
+					<!-- /.box-body -->
+					<div class="box-footer clearfix">
+						<a href="../chat.php" class="btn btn-sm btn-primary pull-right">Go to chat</a>
+					</div>
+					<!-- /.box-footer -->
+				</div>
+				<!-- /.box -->
+			</div>
 			<!-- /.col -->
 		</div>
 		
