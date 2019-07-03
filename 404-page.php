ffd8b49b456b5dbc9d994c7831d187d4a6bac4ff
Instabash <johnathan.lobo@gmail.com>
2019-07-03 14:12:16 +0500

    changed link to index.php

--- 404-page.php
+++ 404-page.php
@@ -6,7 +6,7 @@
 		<h2 class="section-head">
 			<h1 class="">404 Page not found</h1>
 		</h2><br>
-		<p class="lead">The page you were looking for does not exist! Click <a href="../index.php">here</a> to return to the Home page. <br>If you got to this page by clicking a link, please email us so we can fix this issue.</p>
+		<p class="lead">The page you were looking for does not exist! Click <a href="index.php">here</a> to return to the Home page. <br>If you got to this page by clicking a link, please email us so we can fix this issue.</p>
 		
 	</div>
 </section>
