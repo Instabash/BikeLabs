2c2f4f93ae07b4871d7f636d1ea418066f9781aa
Instabash <johnathan.lobo@gmail.com>
2019-07-11 00:09:07 +0500

    added constants for dropdown error handling

---
+++ includes/constants.inc.php
@@ -0,0 +1,14 @@
+<?php
+class constantsinc {
+	
+			public $bikeModels = array("70cc", "125cc", "150cc");
+			public $bikeMakes = array("Honda", "SuperPower", "Unique", "Superstar", "Yamaha", "United", "Suzuki");
+			public $bikeConds = array("New", "Used");
+			public $modPaints = array("Black", "White", "Blue", "Grey", "Red", "Yellow", "Purple", "Green");
+			public $modThemes = array("Flaming skulls theme", "Harley davidson theme", "Batman theme", "Superman theme", "Ironman theme");
+			public $addTitle = array("Mr", "Mrs", "Miss", "Ms", "Dr", "Rev", "Other");
+			public $addCountry = array("IS", "KHI", "LH", "RW", "PSW");
+			public $addStores = array("Yamaha Capital Motors", "Fazal Traders", "Chaudhary Autos", "Qazi Autos", "Paradise Motorcycle");
+			
+			
+}
